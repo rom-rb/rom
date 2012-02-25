86e6794443b054f4caf261da3d4721afead71322
Markus Schirp <mbj@seonic.net>
2012-02-25 22:33:38 +0100

    Add unit specs for Session#untrack

---
+++ spec/unit/session/untrack.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Session::Session,'#untrack' do
+  let(:mapper)       { DummyMapper.new  }
+  let(:mapper_root)  { DummyMapperRoot.new(mapper)  }
+
+  let(:object) do 
+    described_class.new(mapper_root)
+  end
+
+  let(:domain_object) { DomainObject.new }
+
+  subject { object.untrack(domain_object) }
+
+  context 'when object was tracked' do
+    before do
+      object.insert_now(domain_object)
+    end
+
+    it 'should untrack object' do
+      subject
+      object.track?(domain_object).should be_false
+    end
+  end
+end
