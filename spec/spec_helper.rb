448273a390ca64f51d49d0b8a2994c94b1d62b94
Markus Schirp <mbj@seonic.net>
2012-07-12 16:33:32 +0200

    Fix spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ end
 $LOAD_PATH << File.expand_path('../lib', __FILE__)
 
 Dir.glob('spec/examples/**/*.rb').each { |file| require File.expand_path(file) }
-Dir[File.expand_path('../{support, shared}/**/*.rb', __FILE__)].each { |f| require f }
+Dir[File.expand_path('../{support,shared}/**/*.rb', __FILE__)].each { |f| require f }
 
 require 'session'
 require 'session/registry'
