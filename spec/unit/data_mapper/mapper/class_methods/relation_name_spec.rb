46fa780430399d2481ba280a75af2d7f9a577af5
Piotr Solnica <piotr.solnica@gmail.com>
2012-09-01 12:16:27 +0200

    Add test for Mapper.relation_name

---
+++ spec/unit/data_mapper/mapper/class_methods/relation_name_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe DataMapper::Mapper, '.relation_name' do
+  let(:name) { :users }
+
+  context "with a name" do
+    it "sets the relation name" do
+      described_class.relation_name(name)
+      described_class.relation_name.should be(name)
+    end
+  end
+end
