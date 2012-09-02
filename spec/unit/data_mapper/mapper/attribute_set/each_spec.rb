18c63372c2d8cfd3486f39f8a912ba663379a5aa
Piotr Solnica <piotr.solnica@gmail.com>
2012-09-02 13:07:39 +0200

    Fix AttributeSet#each spec

--- spec/unit/data_mapper/mapper/attribute_set/each_spec.rb
+++ spec/unit/data_mapper/mapper/attribute_set/each_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 describe DataMapper::Mapper::AttributeSet, '#each' do
+  subject { attributes.each.to_a }
+
   let(:attributes) { described_class.new }
   let(:id)         { mock('id',   :name => :id) }
   let(:name)       { mock('name', :name => :name) }
 
   before { attributes << id << name }
 
-  it "yields its attributes" do
-    attributes.each.to_a.should eql([ name, id ])
-  end
+  it { should have(2).items }
+  it { should include(id) }
+  it { should include(name) }
 end
