b8e8dafc902a837e1c64df417194383e451a5f46
Piotr Solnica <piotr.solnica@gmail.com>
2012-09-02 12:53:00 +0200

    Fix Attribute#load test

--- spec/unit/data_mapper/mapper/attribute/load_spec.rb
+++ spec/unit/data_mapper/mapper/attribute/load_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 describe DataMapper::Mapper::Attribute, '#load' do
+  subject { attribute.load({}) }
+
   let(:attribute) { described_class.new(:title) }
 
   specify do
-    expect { attribute.load({}).to raise_error(
-      NotImplementedError, "#{described_class} must implement #load")}
+    expect { subject }.to raise_error(
+      NotImplementedError, "#{described_class} must implement #load")
   end
 end
