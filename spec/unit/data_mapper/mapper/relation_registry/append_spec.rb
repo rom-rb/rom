5f8ec8823d568f1ed38a0b914609761f6f0476ba
Piotr Solnica <piotr.solnica@gmail.com>
2012-09-09 11:27:55 +0200

    Add test for RelationRegistry#<<

---
+++ spec/unit/data_mapper/mapper/relation_registry/append_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe DataMapper::Mapper::RelationRegistry, '#<<' do
+  let(:name)     { :users }
+  let(:relation) { mock('relation', :name => name) }
+  let(:registry) { described_class.new }
+
+  it "adds relation to the registry" do
+    registry << relation
+    registry[name].should be(relation)
+  end
+end
