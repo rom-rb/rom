943a67892a12ec2c441f06f527bc996f0dd040bb
Piotr Solnica <piotr.solnica@gmail.com>
2012-09-09 11:11:18 +0200

    Add test for MapperRegistry#<<

---
+++ spec/unit/data_mapper/mapper/mapper_registry/append_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe DataMapper::Mapper::MapperRegistry, '#<<' do
+  let(:model)    { mock('model') }
+  let(:mapper)   { mock_mapper(model).new }
+  let(:registry) { described_class.new }
+
+  it "adds a new mapper to the registry" do
+    registry << mapper
+    registry[model].should be(mapper)
+  end
+end
