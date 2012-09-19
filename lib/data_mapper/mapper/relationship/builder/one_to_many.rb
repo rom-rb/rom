150acca65990dd6270fe53036b4aeb6976968117
snusnu <gamsnjaga@gmail.com>
2012-09-19 14:38:00 +0200

    Initial support for autogenerated 1:1 mappers

--- lib/data_mapper/mapper/relationship/builder/one_to_many.rb
+++ lib/data_mapper/mapper/relationship/builder/one_to_many.rb
@@ -3,28 +3,8 @@ module DataMapper
     class Relationship
       class Builder
 
-        class OneToMany < self
-
+        class OneToMany < OneToOne
           include CollectionBehavior
-
-          def operation
-            lambda do |targets, relationship|
-              source_key = relationship.source_key
-              target_key = relationship.target_key
-
-              rename(relationship.options[:renamings].merge({
-                source_key => target_key
-              })).join(targets)
-            end
-          end
-
-          private
-
-          def fields
-            super.merge({
-              source_key => target_key,
-            })
-          end
         end # class OneToMany
       end # class Builder
     end # class Relationship
--- lib/data_mapper/mapper/relationship/builder/one_to_one.rb
+++ lib/data_mapper/mapper/relationship/builder/one_to_one.rb
@@ -3,8 +3,24 @@ module DataMapper
     class Relationship
       class Builder
 
-          # TODO implement
         class OneToOne < self
+
+          def operation
+            lambda do |targets, relationship|
+              source_key = relationship.source_key
+              target_key = relationship.target_key
+
+              rename(relationship.options[:renamings].merge({
+                source_key => target_key
+              })).join(targets)
+            end
+          end
+
+          private
+
+          def fields
+            super.merge(source_key => target_key)
+          end
         end # class OneToOne
       end # class Builder
     end # class Relationship
