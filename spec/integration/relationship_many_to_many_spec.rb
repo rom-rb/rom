761e8934c10053accbc78a181b3522beedf7fce1
snusnu <gamsnjaga@gmail.com>
2012-10-16 17:49:35 +0200

    Mark m2m integration spec as pending for 1.8.7

--- spec/integration/relationship_many_to_many_spec.rb
+++ spec/integration/relationship_many_to_many_spec.rb
@@ -123,6 +123,8 @@ describe 'Relationship - Many To Many with generated mappers' do
   end
 
   it 'loads associated tags with name = good' do
+    pending if RUBY_VERSION < '1.9'
+
     mapper = DataMapper[Song]
     songs = mapper.include(:good_tags).to_a
 
