ff05d22ce5828ab4b8eb3563d8fe196290248f80
Piotr Solnica <piotr.solnica@gmail.com>
2012-10-19 08:28:26 +0200

    Refactor Connector::Builder.call

--- lib/data_mapper/relation_registry/connector/builder.rb
+++ lib/data_mapper/relation_registry/connector/builder.rb
@@ -2,55 +2,109 @@ module DataMapper
   class RelationRegistry
     class Connector
 
+      # Builds connectors and register relation nodes for a given relationship
+      #
       class Builder
+        attr_reader :mappers
+        attr_reader :relations
+        attr_reader :relationship
+        attr_reader :connector
 
-        def self.call(mappers, relations, relationship)
-          left_mapper  = mappers[relationship.source_model]
-          right_mapper = mappers[relationship.target_model]
+        # @api public
+        def self.call(*args)
+          new(*args).connector
+        end
 
-          left_node =
-            if relationship.via
-              via_relationship = left_mapper.relationships[relationship.via]
-              via_name         = :"#{left_mapper.relation.name}_#{via_relationship.name}"
+        # @api private
+        def initialize(mappers, relations, relationship)
+          @mappers, @relations, @relationship = mappers, relations, relationship
+          initialize_connector
+          freeze
+        end
 
-              via_relation =
-                if relations[via_name]
-                  relations[via_name]
-                else
-                  call(mappers, relations, via_relationship).aliased_for(relationship)
-                end.relation
+        private
 
-              relations.build_node(via_name, via_relation)
-            else
-              relations.node_for(left_mapper.relation)
-            end
-
-          right_node =
-            if via_relationship
-              relations.node_for(right_mapper.relation).aliased_for(via_relationship)
-            else
-              relations.node_for(right_mapper.relation)
-            end
+        # @api private
+        def initialize_connector
+          left_node  = build_left_node
+          right_node = build_right_node
 
           unless left_node && right_node
             raise ArgumentError, "Missing left and/or right nodes for #{relationship.name} left: #{left_node.inspect} right: #{right_node.inspect}"
           end
 
-          relations.add_node(left_node)  unless relations[left_node.name]
-          relations.add_node(right_node) unless relations[right_node.name]
+          @connector = Connector.new(build_edge(left_node, right_node), relationship)
 
+          relations.add_connector(@connector)
+        end
+
+        # @api private
+        def left_mapper
+          @left_mapper ||= mappers[relationship.source_model]
+        end
+
+        # @api private
+        def right_mapper
+          @right_mapper ||= mappers[relationship.target_model]
+        end
+
+        # @api private
+        def via?
+          relationship.via
+        end
+
+        # @api private
+        def via_relationship
+          @via_relationship ||= left_mapper.relationships[relationship.via]
+        end
+
+        # @api private
+        def via_name
+          @via_name ||= :"#{left_mapper.relation.name}_#{via_relationship.name}"
+        end
+
+        # @api private
+        def via_relation
+          @via_relation ||=
+            if relations[via_name]
+              relations[via_name]
+            else
+              self.class.call(mappers, relations, via_relationship).aliased_for(relationship)
+            end.relation
+        end
+
+        # @api private
+        def build_left_node
+          if via?
+            node = relations.build_node(via_name, via_relation)
+            relations.add_node(node)
+            node
+          else
+            relations.node_for(left_mapper.relation)
+          end
+        end
+
+        # @api private
+        def build_right_node
+          if via?
+            node = relations.node_for(right_mapper.relation).aliased_for(via_relationship)
+            relations.add_node(node)
+            node
+          else
+            relations.node_for(right_mapper.relation)
+          end
+        end
+
+        # @api private
+        def build_edge(left, right)
           edge = relations.edges.detect { |e| e.name == relationship.name }
 
           unless edge
-            edge = relations.build_edge(relationship.name, left_node, right_node)
+            edge = relations.build_edge(relationship.name, left, right)
             relations.add_edge(edge)
           end
 
-          connector = Connector.new(edge, relationship)
-
-          relations.add_connector(connector)
-
-          connector
+          edge
         end
 
       end # class Builder
