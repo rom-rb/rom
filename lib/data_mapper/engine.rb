bfa7dd2667fb6e3c222f3c7473da696665073e11
Piotr Solnica <piotr.solnica@gmail.com>
2012-10-28 18:15:12 +0100

    And even more TODOs

--- lib/data_mapper/engine.rb
+++ lib/data_mapper/engine.rb
@@ -4,27 +4,32 @@ module DataMapper
     attr_reader :relations
 
     # @api private
+    # TODO: add specs
     def initialize(uri = nil)
       @uri       = uri
       @relations = RelationRegistry.new(self)
     end
 
     # @api public
+    # TODO: add specs
     def relation_node_class
       RelationRegistry::RelationNode
     end
 
     # @api public
+    # TODO: add specs
     def relation_edge_class
       RelationRegistry::RelationEdge
     end
 
     # @api public
+    # TODO: add specs
     def base_relation(name)
       raise NotImplementedError, "#{self.class}##{__method__} must be implemented"
     end
 
     # @api public
+    # TODO: add specs
     def gateway_relation(relation)
       relation
     end
