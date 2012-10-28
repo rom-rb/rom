30f57944de0f0cb3083a50b67e598de2d5da0a50
Piotr Solnica <piotr.solnica@gmail.com>
2012-10-28 09:42:00 +0100

    Simplify AliasSet#each

--- lib/data_mapper/alias_set.rb
+++ lib/data_mapper/alias_set.rb
@@ -3,11 +3,10 @@ module DataMapper
     include Enumerable
 
     attr_reader :prefix
-
     attr_reader :attributes
-
     attr_reader :excluded
 
+    # @api private
     def initialize(prefix, attributes = Mapper::AttributeSet.new, excluded = [])
       @prefix     = prefix
       @attributes = attributes
@@ -15,28 +14,33 @@ module DataMapper
       initialize_index(attributes)
     end
 
+    # @api private
     def each(&block)
       return to_enum unless block_given?
-      @index.each { |field, alias_name| yield(field, alias_name) }
+      @index.each(&block)
       self
     end
 
+    # @api private
     def exclude(*names)
       self.class.new(prefix, attributes, excluded.dup.concat(names))
     end
 
+    # @api private
     def merge(other)
       attributes = @attributes.merge(other.attributes)
       excluded   = @excluded.dup.concat(other.excluded)
       self.class.new(prefix, attributes, excluded)
     end
 
+    # @api private
     def to_hash
       @index
     end
 
     private
 
+    # @api private
     def initialize_index(attributes)
       @index = attributes.primitives.each_with_object({}) do |attribute, fields|
         next if excluded?(attribute)
@@ -45,10 +49,12 @@ module DataMapper
       end
     end
 
+    # @api private
     def excluded?(attribute)
       @excluded.include?(attribute.name)
     end
 
+    # @api private
     def alias_for(field)
       :"#{@prefix}_#{field}"
     end
