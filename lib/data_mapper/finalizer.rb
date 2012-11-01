dfe1b0aa825ed84797373fcbbe6b9ad662977321
snusnu <gamsnjaga@gmail.com>
2012-11-01 20:09:00 +0100

    Add docs for DataMapper::Finalizer

--- lib/data_mapper/finalizer.rb
+++ lib/data_mapper/finalizer.rb
@@ -1,17 +1,63 @@
 module DataMapper
 
+  # Creates mappers for join relations described by relationship definitions
+  #
   # TODO: split this giant into smaller objects and add proper unit specs
   class Finalizer
+
+    # The mapper registry in use
+    #
+    # @return [MapperRegistry]
+    #
+    # @api private
     attr_reader :mapper_registry
+
+    # The edge builder in use
+    #
+    # @return [RelationRegistry::Builder]
+    #
+    # @api private
     attr_reader :edge_builder
+
+    # The mapper builder in use
+    #
+    # @return [Mapper::Builder]
+    #
+    # @api private
     attr_reader :mapper_builder
+
+    # The mappers to be finalized
+    #
+    # @return [Enumerable<Mapper>]
+    #
+    # @api private
     attr_reader :mappers
 
+    # Perform finalization
+    #
+    # @example
+    #   DataMapper::Finalizer.run
+    #
+    # @return [self]
+    #
     # @api public
     def self.run
       new(Mapper.descendants.select { |mapper| mapper.model }).run
     end
 
+    # Initialize a new finalizer instance
+    #
+    # @param [Enumerable<Mapper>] mappers
+    #   the mappers to be finalized
+    #
+    # @param [RelationRegistry::Builder] edge_builder
+    #   the builder used to create edges for relationships
+    #
+    # @param [Mapper::Builder] mapper_builder
+    #   the builder used to create mappers for relationships
+    #
+    # @return [undefined]
+    #
     # @api private
     def initialize(mappers, edge_builder = RelationRegistry::Builder, mapper_builder = Mapper::Builder)
       @mappers         = mappers
@@ -22,6 +68,10 @@ module DataMapper
       @base_relation_mappers = @mappers.select { |mapper| mapper.respond_to?(:relation_name) }
     end
 
+    # Perform finalization
+    #
+    # @return [self]
+    #
     # @api private
     def run
       finalize_base_relation_mappers
