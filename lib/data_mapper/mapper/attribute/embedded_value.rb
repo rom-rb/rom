3b783e1359fd1b97b3c4b4755afa6178a1c1bc65
snusnu <gamsnjaga@gmail.com>
2012-11-01 22:40:21 +0100

    Add docs for Mapper::Attribute::EmbeddedValue

--- lib/data_mapper/mapper/attribute/embedded_value.rb
+++ lib/data_mapper/mapper/attribute/embedded_value.rb
@@ -3,34 +3,74 @@ module DataMapper
     class Attribute
 
       class EmbeddedValue < Attribute
+
+        # Error raised when type option is missing
         MissingTypeOptionError = Class.new(StandardError)
 
+        # The mapper used for mapping this embedded value attribute
+        #
+        # @return [Mapper]
+        #
+        # @api private
         attr_reader :mapper
 
+        # Initialize a new embedded value attribute instance
+        #
+        # @see Attribute#initialize
+        #
+        # @return [undefined]
+        #
+        # @api private
         def initialize(name, options = {})
           super
           @type    = options.fetch(:type) { raise(MissingTypeOptionError) }
           @aliases = options.fetch(:aliases, {})
         end
 
-        # @api public
+        # Finalize this embedded value attribute
+        #
+        # @return [self]
+        #
+        # @api private
         def finalize
           mapper = DataMapper[type]
           mapper = mapper.remap(@aliases) if @aliases.any?
 
           @mapper = mapper
+
+          self
         end
 
+        # The attribute's human readable representation
+        #
+        # @example
+        #
+        #   attribute = DataMapper[Person].attributes[:name]
+        #   puts attribute.inspect
+        #
+        # @return [String]
+        #
         # @api public
         def inspect
           "<##{self.class.name} @name=#{name} @mapper=#{mapper}>"
         end
 
-        # @api public
+        # Load this attribute's value from a tuple
+        #
+        # @param [(#each, #[])] tuple
+        #   the tuple to load
+        #
+        # @return [Object]
+        #
+        # @api private
         def load(tuple)
           mapper.load(tuple)
         end
 
+        # Tests wether this attribute is primitive or not
+        #
+        # @return [Boolean] false
+        #
         # @api private
         def primitive?
           false
