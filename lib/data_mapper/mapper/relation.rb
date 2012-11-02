1e8944998e7bc27329d7a01f9b85cdb97b56f0a3
snusnu <gamsnjaga@gmail.com>
2012-11-02 01:57:34 +0100

    Add docs for DataMapper::Mapper::Relation

--- lib/data_mapper/mapper/relation.rb
+++ lib/data_mapper/mapper/relation.rb
@@ -7,9 +7,30 @@ module DataMapper
     class Relation < self
       alias_method :all, :to_a
 
+      # The relation backing this mapper
+      #
+      # @example
+      #
+      #   mapper = DataMapper[Person]
+      #   mapper.relation
+      #
+      # @return [RelationRegistry::RelationNode]
+      #
+      # @api public
       attr_reader :relation
 
+      # Return a new mapper class derived from the given one
+      #
       # @see Mapper.from
+      #
+      # @example
+      #
+      #   other = DataMapper[Person].class
+      #   DataMapper::Mapper::Relation.from(other, 'AdminMapper')
+      #
+      # @return [Mapper::Relation]
+      #
+      # @api public
       def self.from(other, name = nil)
         klass = super
         klass.repository(other.repository)
@@ -28,6 +49,12 @@ module DataMapper
 
       # Returns relation registry for this mapper class
       #
+      # @see Engine#relations
+      #
+      # @example
+      #
+      #   DataMapper::Mapper::Relation.relations
+      #
       # @return [RelationRegistry]
       #
       # @api public
@@ -36,6 +63,10 @@ module DataMapper
       end
 
       # Returns base relation for this mapper
+      #
+      # @example
+      #
+      #   DataMapper::Mapper::Relation.relation
       #
       # @return [Object]
       #
@@ -53,7 +84,23 @@ module DataMapper
         @gateway_relation ||= engine.gateway_relation(relation)
       end
 
-      # Sets or returns the name of this mapper's repository
+      # Set or return the name of this mapper's repository
+      #
+      # @example when setting the repository name
+      #
+      #   class UserMapper
+      #     repository :foo
+      #   end
+      #
+      # @example when reading the repository name
+      #
+      #   mapper = DataMapper[User]
+      #   mapper.class.repository
+      #
+      # @param [Symbol] name
+      #   the repository name
+      #
+      # @return [Symbol, nil]
       #
       # @api public
       def self.repository(name = Undefined)
@@ -64,7 +111,23 @@ module DataMapper
         end
       end
 
-      # Sets or returns the name of this mapper's relation
+      # Set or return the name of this mapper's relation
+      #
+      # @example when setting the relation name
+      #
+      #   class UserMapper
+      #     relation_name :users
+      #   end
+      #
+      # @example when reading the relation name
+      #
+      #   mapper = DataMapper[User]
+      #   mapper.class.relation_name
+      #
+      # @param [Symbol] name
+      #   the relation name
+      #
+      # @return [Symbol, nil]
       #
       # @api public
       def self.relation_name(name = Undefined)
@@ -75,6 +138,24 @@ module DataMapper
         end
       end
 
+      # Mark the given attribute names as (part of) the key
+      #
+      # @example
+      #
+      #   class Person
+      #     include DataMapper::Model
+      #     attribute :id, Integer
+      #   end
+      #
+      #   DataMapper.generate_mapper_for(Person, :postgres) do
+      #     key :id
+      #   end
+      #
+      # @param [(Symbol)] *names
+      #   the attribute names that together consitute the key
+      #
+      # @return [self]
+      #
       # @api public
       def self.key(*names)
         names.each do |name|
@@ -82,11 +163,17 @@ module DataMapper
         end
       end
 
+      # The aliases used for this mapper's instances
+      #
+      # @return [AliasSet]
+      #
       # @api private
       def self.aliases
         @aliases ||= AliasSet.new(Inflector.singularize(relation_name), attributes)
       end
 
+      # Perform finalization
+      #
       # @api private
       def self.finalize
         Mapper.mapper_registry << new(relations.node_for(gateway_relation))
@@ -94,7 +181,21 @@ module DataMapper
 
       # Initialize a veritas mapper instance
       #
-      # @param [Veritas::Relation]
+      # @example
+      #
+      #   class PersonMapper < DataMapper::Mapper::Relation
+      #     relation_name :people
+      #     model Person
+      #     repository :postgres
+      #   end
+      #
+      #   mapper = PersonMapper.new
+      #
+      # @param [Veritas::Relation] relation
+      #   the relation to map from
+      #
+      # @param [AttributeSet] attributes
+      #   the set of attributes to map
       #
       # @return [undefined]
       #
@@ -105,11 +206,35 @@ module DataMapper
         @attributes = attributes
       end
 
+      # Return a new instance with mapping that corresponds to aliases
+      #
       # TODO find a better name
+      #
+      # @param [Hash] aliases
+      #   the aliases to use in the returned instance
+      #
+      # @return [Mapper::Relation]
+      #
+      # @api private
       def remap(aliases)
         self.class.new(@relation, @attributes.remap(aliases))
       end
 
+      # Iterate over the loaded domain objects
+      #
+      # @example
+      #
+      #   DataMapper[Person].each do |person|
+      #     puts person.name
+      #   end
+      #
+      # @yield [object] the loaded domain objects
+      #
+      # @yieldparam [Object] object
+      #   the loaded domain object that is yielded
+      #
+      # @return [self]
+      #
       # @api public
       def each
         return to_enum unless block_given?
@@ -117,27 +242,87 @@ module DataMapper
         self
       end
 
+      # The mapped relation's name
+      #
+      # @see Mapper::Relation.relation_name
+      #
+      # @example
+      #
+      #   mapper = DataMapper[Person]
+      #   mapper.relation_name
+      #
+      # @return [Symbol]
+      #
       # @api public
       def relation_name
         self.class.relation_name
       end
 
+      # The mapper's model
+      #
+      # @see Mapper::Relation.model
+      #
+      # @example
+      #
+      #   mapper = DataMapper[Person]
+      #   mapper.model
+      #
+      # @return [::Class]
+      #   a domain model class
+      #
       # @api public
       def model
         self.class.model
       end
 
+      # The mapper's human readable representation
+      #
+      # @example
+      #
+      #   mapper = DataMapper[Person]
+      #   puts mapper.inspect
+      #
+      # @return [String]
+      #
       # @api public
       def inspect
         "<##{self.class.name}:#{object_id} @model=#{@model} @repository=#{self.class.repository} @relation=#{@relation}>"
       end
 
+      # Return a mapper for iterating over the relation restricted with options
+      #
+      # @see Veritas::Relation#restrict
+      #
+      # @example
+      #
+      #   mapper = DataMapper[Person]
+      #   mapper.find(:name => 'John').to_a
+      #
+      # @param [Hash] options
+      #   the options to restrict the relation
+      #
+      # @return [Mapper::Relation]
+      #
       # @api public
       def find(options)
         restriction = @relation.restrict(Query.new(options, @attributes))
         self.class.new(restriction)
       end
 
+      # Return a mapper for iterating over the relation ordered by *order
+      #
+      # @see Veritas::Relation#sort_by
+      #
+      # @example
+      #
+      #   mapper = DataMapper[Person]
+      #   mapper.order(:name).to_a
+      #
+      # @param [(Symbol)] *order
+      #   the attribute names to order by
+      #
+      # @return [Mapper::Relation]
+      #
       # @api public
       def order(*order)
         attributes = order.map { |attribute|
@@ -153,6 +338,22 @@ module DataMapper
         self.class.new(sorted)
       end
 
+      # Return a mapper for iterating over the relation ordered by *order
+      #
+      # @example
+      #
+      #   mapper = DataMapper[Person]
+      #   mapper.one(:name => 'John')
+      #
+      # @param [Hash] options
+      #   the options to restrict the relation
+      #
+      # @raise RuntimeError
+      #   if more than one domain object was found
+      #
+      # @return [Object]
+      #   a domain object
+      #
       # @api public
       def one(options = {})
         results = find(options).to_a
@@ -165,26 +366,95 @@ module DataMapper
         end
       end
 
+      # Return a mapper for iterating over domain objects with loaded relationships
+      #
+      # @example
+      #
+      #   DataMapper[Person].include(:tasks).each do |person|
+      #     person.tasks.each do |task|
+      #       puts task.name
+      #     end
+      #   end
+      #
+      # @param [Symbol] name
+      #   the name of the relationship to include
+      #
+      # @return [Mapper::Relation]
+      #
       # @api public
       def include(name)
         Mapper.mapper_registry[self.class.model, relationships[name]]
       end
 
+      # Return a mapper for iterating over a restricted set of domain objects
+      #
+      # @example
+      #
+      #   DataMapper[Person].restrict { |r| r.name.eq('John') }.each do |person|
+      #     puts person.name
+      #   end
+      #
+      # @param [Proc] &block
+      #   the block to restrict the relation with
+      #
+      # @return [Mapper::Relation]
+      #
       # @api public
       def restrict(&block)
         self.class.new(@relation.restrict(&block))
       end
 
+      # Return a mapper for iterating over a sorted set of domain objects
+      #
+      # @example
+      #
+      #   DataMapper[Person].sort_by { |r| [ r.name.desc ] }.each do |person|
+      #     puts person.name
+      #   end
+      #
+      # @param [Proc] &block
+      #   the block to evaluate for the sort directions
+      #
+      # @return [Mapper::Relation]
+      #
       # @api public
       def sort_by(&block)
         self.class.new(@relation.sort_by(&block))
       end
 
+      # Return a mapper for iterating over domain objects with renamed attributes
+      #
+      # @example
+      #
+      #   DataMapper[Person].rename(:name => :nickname).each do |person|
+      #     puts person.nickname
+      #   end
+      #
+      # @param [Hash] aliases
+      #   the old and new attribute names as alias pairs
+      #
+      # @return [Mapper::Relation]
+      #
       # @api public
-      def rename(mapping, &block)
-        self.class.new(@relation.rename(mapping, &block))
+      def rename(aliases, &block)
+        self.class.new(@relation.rename(aliases, &block))
       end
 
+      # Return a mapper for iterating over the result of joining other with self
+      #
+      # TODO investigate if the following example works
+      #
+      # @example
+      #
+      #   DataMapper[Person].join(DataMapper[Task]).each do |person|
+      #     puts person.tasks.size
+      #   end
+      #
+      # @param [Mapper::Relation] other
+      #   the other mapper to join with self
+      #
+      # @return [Mapper::Relation]
+      #
       # @api public
       def join(other)
         self.class.new(@relation.join(other.relation))
