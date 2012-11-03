e7a939c96323ba0fc215fa3429090edf6e2bd663
Piotr Solnica <piotr.solnica@gmail.com>
2012-11-03 13:45:34 +0100

    Add specs for Mapper::Relation#each

--- spec/unit/data_mapper/mapper/relation/each_spec.rb
+++ spec/unit/data_mapper/mapper/relation/each_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Mapper::Relation, '#each' do
-  it 'needs spec'
+  let(:object)   { Class.new(described_class).model(model).new(relation) }
+  let(:model)    { mock_model(:User) }
+  let(:relation) { mock('relation') }
+
+  context "without a block" do
+    subject { object.each }
+
+    it { should be_instance_of(Enumerator) }
+  end
+
+  context "with a block" do
+    subject { object.each(&block) }
+
+    let(:block) { Proc.new {} }
+    let(:tuple) { {} }
+
+    it "iterates over relation tuples and loads them" do
+      relation.should_receive(:each).with(&block).and_yield(tuple)
+      model.should_receive(:new).with(tuple)
+      subject
+    end
+  end
 end
