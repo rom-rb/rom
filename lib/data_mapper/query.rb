80f01569c55d59747d98bafc2174e95f7810e417
Piotr Solnica <piotr.solnica@gmail.com>
2012-11-04 11:16:37 +0100

    Add specs and docs for Query

--- lib/data_mapper/query.rb
+++ lib/data_mapper/query.rb
@@ -1,19 +1,39 @@
 module DataMapper
+
+  # Represents a query for a restriction
+  #
   class Query
     include Enumerable
 
+    # Initializes a new query instance
+    #
+    # @param [Hash]
+    #
+    # @param [Mapper::AttributeSet]
+    #
+    # @return [undefined]
+    #
+    # @api private
     def initialize(options, attributes)
       @options    = options
       @attributes = attributes
     end
 
-    # @api public
+    # Iterate over attribute fields
+    #
+    # @return [self]
+    #
+    # @api private
     def each
       return to_enum unless block_given?
+
       @options.each do |name, value|
         yield(@attributes.field_name(name), value)
       end
+
       self
     end
-  end
+
+  end # class Query
+
 end # module DataMapper
---
+++ spec/unit/data_mapper/query/each_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Query, '#each' do
+  let(:object) { described_class.new(options, attributes) }
+
+  let(:options)    { { :name => 'Piotr', :age => 29 } }
+  let(:attributes) { Mapper::AttributeSet.new         }
+
+  let(:name) { Mapper::Attribute.build(:name, :type => String,  :to => :user_name) }
+  let(:age)  { Mapper::Attribute.build(:age,  :type => Integer, :to => :user_age)  }
+
+  before { attributes << name << age }
+
+  context "with a block" do
+    subject { object.to_a }
+
+    it { should have(2).items }
+    it { should include([:user_name, 'Piotr']) }
+    it { should include([:user_age, 29]) }
+  end
+
+  context "without a block" do
+    subject { object.each }
+
+    it { should be_instance_of(Enumerator) }
+  end
+end
