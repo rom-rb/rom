bb3c5f42179035d79bc7bdacb082afea2e6c3150
snusnu <gamsnjaga@gmail.com>
2012-11-06 02:10:46 +0100

    Add specs for DataMapper::Mapper#model

---
+++ spec/unit/data_mapper/mapper/model_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Mapper, '#model' do
+  subject { object.model }
+
+  let(:object) { described_class.new }
+  let(:model)  { mock('model') }
+
+  it 'delegates to self.class' do
+    described_class.should_receive(:model).and_return(model)
+    subject.should be(model)
+  end
+end
