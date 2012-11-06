7835a6509dd251507284a1b3a4776c887c2e5040
snusnu <gamsnjaga@gmail.com>
2012-11-06 17:15:27 +0100

    Add specs for DataMapper::Relationship#via

---
+++ spec/unit/data_mapper/relationship/via_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Relationship, '#via' do
+  subject { object.via }
+
+  let(:object)       { described_class.new(name, source_model, target_model, options) }
+  let(:name)         { :songs }
+  let(:source_model) { mock('source_model') }
+  let(:target_model) { mock('target_model') }
+
+  context 'when :through is not present in options' do
+    let(:options) { {} }
+
+    it { should be(nil) }
+  end
+
+  context 'when :through is present in options' do
+    let(:options) { { :through => via } }
+    let(:via)     { :song_tags }
+
+    it { should be(via) }
+  end
+end
