201ce87ebae74c21ea14a495db8101fb9d4e2d80
snusnu <gamsnjaga@gmail.com>
2012-11-06 18:13:03 +0100

    Add specs for Mapper::Relation#remap

---
+++ spec/unit/data_mapper/mapper/relation/remap_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Mapper::Relation, '#remap' do
+  subject { object.remap(aliases) }
+
+  let(:object)     { mock_mapper(model, attributes).new(relation) }
+  let(:model)      { mock_model(:User) }
+  let(:attributes) { [ mock_attribute(:id, Integer) ] }
+  let(:relation)   { mock('relation') }
+  let(:aliases)    { { :id => :user_id } }
+
+  it 'does not change the number of attributes' do
+    subject.attributes.count.should == 1
+  end
+
+  it 'returns a new instance with aliased attributes' do
+    subject.attributes[:id].field.should be(:user_id)
+  end
+end
