c5c6a639d46d47639f742a99e0e0b46fc52ea447
snusnu <gamsnjaga@gmail.com>
2012-11-06 03:51:06 +0100

    Add docs for Graph::Edge

--- lib/data_mapper/support/graph/edge.rb
+++ lib/data_mapper/support/graph/edge.rb
@@ -2,10 +2,62 @@ class Graph
   class Edge
     include Equalizer.new(:name, :left, :right)
 
+    # The edge name
+    #
+    # @example
+    #
+    #   left  = Node.new(:left)
+    #   right = Node.new(:right)
+    #   edge = Edge.new(:name, left, right)
+    #   edge.name
+    #
+    # @return [Symbol]
+    #
+    # @api public
     attr_reader :name
+
+    # The edge's left {Node}
+    #
+    # @example
+    #
+    #   left  = Node.new(:left)
+    #   right = Node.new(:right)
+    #   edge = Edge.new(:name, left, right)
+    #   edge.left
+    #
+    # @return [Node]
+    #
+    # @api public
     attr_reader :left
+
+    # The edge's right {Node}
+    #
+    # @example
+    #
+    #   left  = Node.new(:left)
+    #   right = Node.new(:right)
+    #   edge = Edge.new(:name, left, right)
+    #   edge.right
+    #
+    # @return [Node]
+    #
+    # @api public
     attr_reader :right
 
+    # Initialize a new edge
+    #
+    # @param [#to_sym] name
+    #   the edge's name
+    #
+    # @param [Node] left
+    #   the edge's left node
+    #
+    # @param [Node] right
+    #   the edge's right node
+    #
+    # @return [undefined]
+    #
+    # @api private
     def initialize(name, left, right)
       @name  = name.to_sym
       @left  = left
@@ -13,6 +65,22 @@ class Graph
       @nodes = Set[ left, right ]
     end
 
+    # Tests wether this edge connects the given {Node}
+    #
+    # @example
+    #
+    #   left  = Node.new(:left)
+    #   right = Node.new(:right)
+    #   edge = Edge.new(:name, left, right)
+    #   edge.connects?(left)
+    #
+    # @param [Node] node
+    #   the node to test
+    #
+    # @return [Boolean]
+    #   true if this edge connects +node+, false otherwise
+    #
+    # @api public
     def connects?(node)
       @nodes.include?(node)
     end
