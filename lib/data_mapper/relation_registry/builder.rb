6f3a83904ee7b6f8ff2577a81b92a554a6af7595
snusnu <gamsnjaga@gmail.com>
2012-11-06 03:22:49 +0100

    Add docs for RelationRegistry::Builder

--- lib/data_mapper/relation_registry/builder.rb
+++ lib/data_mapper/relation_registry/builder.rb
@@ -4,18 +4,70 @@ module DataMapper
     # Builds relation nodes for relationships
     #
     class Builder
+
+      # The {RelationRegistry} used by this builder
+      #
+      # @return [RelationRegistry]
+      #
+      # @api private
       attr_reader :relations
+
+      # The {MapperRegistry} used by this builder
+      #
+      # @return [MapperRegistry]
+      #
+      # @api private
       attr_reader :mappers
+
+      # The relationship to build the {RelationNode} and {Connector} for
+      #
+      # @see RelationNode
+      # @see Connector
+      #
+      # @return [Relationship]
+      #
+      # @api private
       attr_reader :relationship
 
+      # The {Connector} built for {#relationship}
+      #
+      # @return [Connector]
+      #
+      # @api private
       attr_reader :connector
 
+      # Build a new {RelationNode} and {Connector} for +relationship+
+      #
+      # @param [RelationRegistry] relations
+      #   a registry of relations
+      #
+      # @param [MapperRegistry] mappers
+      #   a registry of mappers
+      #
+      # @param [Relationship] relationship
+      #   the relationship the connector is built for
+      #
+      # @return [BaseBuilder, ViaBuilder]
+      #
       # @api private
       def self.call(relations, mappers, relationship)
         klass = relationship.via ? ViaBuilder : BaseBuilder
         klass.new(relations, mappers, relationship)
       end
 
+      # Initialize a new {Builder} instance
+      #
+      # @param [RelationRegistry] relations
+      #   the registry of relations
+      #
+      # @param [MapperRegistry] mappers
+      #   the registry of mappers
+      #
+      # @param [Relationship] relationship
+      #   the relationship the connector is built for
+      #
+      # @return [undefined]
+      #
       # @api private
       def initialize(relations, mappers, relationship)
         @relations, @mappers, @relationship = relations, mappers, relationship
@@ -29,21 +81,37 @@ module DataMapper
         relations.add_connector(@connector)
       end
 
+      # The relationship's source model relation name
+      #
+      # @return [Symbol]
+      #
       # @api private
       def left_name
         mappers[relationship.source_model].class.relation_name
       end
 
+      # The relationship's target model relation name
+      #
+      # @return [Symbol]
+      #
       # @api private
       def right_name
         mappers[relationship.target_model].class.relation_name
       end
 
+      # The relationship's source relation node
+      #
+      # @return [RelationNode]
+      #
       # @api private
       def left_node
         relations[left_name]
       end
 
+      # The relationship's target relation node
+      #
+      # @return [RelationNode]
+      #
       # @api private
       def right_node
         relations[right_name]
