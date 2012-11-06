f411ba261708888e4b9f7cd6b62021660e47ee56
snusnu <gamsnjaga@gmail.com>
2012-11-06 17:15:14 +0100

    Add specs for DataMapper::Relationship#target_key

---
+++ spec/unit/data_mapper/relationship/target_key_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Relationship, '#target_key' do
+  subject { object.target_key }
+
+  let(:object)       { described_class.new(name, source_model, target_model, options) }
+  let(:name)         { :songs }
+  let(:source_model) { mock('source_model') }
+  let(:target_model) { mock('target_model') }
+
+  context 'when :target_key is not present in options' do
+    let(:options) { {} }
+
+    it 'uses #default_target_key' do
+      subject.should be(object.default_target_key)
+    end
+  end
+
+  context 'when :target_key is present in options' do
+    let(:options)    { { :target_key => target_key } }
+    let(:target_key) { :song_id }
+
+    it { should be(target_key) }
+  end
+end
