54a853ec303b47c3c911491f82dabbcd5b315001
snusnu <gamsnjaga@gmail.com>
2012-11-08 19:01:20 +0100

    Add specs for RelationRegistry::Connector#relations

---
+++ spec/unit/data_mapper/relation_registry/connector/relations_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe RelationRegistry::Connector, '#relationship' do
+  subject { object.relationship }
+
+  let(:object) { described_class.new(node, relationship, relations) }
+
+  let(:name)         { :users_X_addresses }
+  let(:node)         { mock('relation_node', :name => name) }
+  let(:relationship) { mock('relationship') }
+  let(:relations)    { mock('relations') }
+
+  it { should equal(relationship) }
+end
