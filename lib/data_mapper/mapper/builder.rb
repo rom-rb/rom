32c6375289bdab9e4396aff95932d87f9d4714f6
snusnu <gamsnjaga@gmail.com>
2012-11-08 17:27:16 +0100

    Make Mapper::Builder build mappers in #initialize

--- lib/data_mapper/mapper/builder.rb
+++ lib/data_mapper/mapper/builder.rb
@@ -1,14 +1,14 @@
 module DataMapper
   class Mapper
 
-    # Builds relationship mappers
+    # Builds a {Mapper::Relation} from a {RelationRegistry::Connector}
     #
+    # @api private
     class Builder
 
       # Builds a mapper based on a connector and source mapper class
       #
       # @param [RelationRegistry::Connector] connector
-      # @param [Class] source mapper
       #
       # @return [Class]
       #
@@ -17,21 +17,35 @@ module DataMapper
         new(connector).mapper
       end
 
+      # The mapper built from the instance's {RelationRegistry::Connector}
+      #
+      # @return [Mapper::Relation]
+      #
+      # @api private
+      attr_reader :mapper
+
+      # Initialize a new instance
+      #
+      # @param [RelationRegistry::Connector] connector
+      #   the connector used to build the mapper
+      #
+      # @return [undefined]
+      #
+      # @api private
       def initialize(connector)
         @connector     = connector
         @source_model  = connector.source_model
         @target_model  = connector.target_model
         @source_mapper = connector.source_mapper.class
         @name          = connector.relationship.name
-      end
 
-      def mapper
-        mapper_class.new(@connector.node)
+        initialize_mapper
       end
 
       private
 
-      def mapper_class
+      # @api private
+      def initialize_mapper
         klass = Mapper::Relation.from(@source_mapper, mapper_name)
 
         remap_fields(klass)
@@ -44,9 +58,10 @@ module DataMapper
 
         klass.finalize_attributes
 
-        klass
+        @mapper = klass.new(@connector.node)
       end
 
+      # @api private
       def remap_fields(mapper)
         source_aliases.each do |field, alias_name|
           attribute = mapper.attributes.for_field(field)
@@ -58,18 +73,22 @@ module DataMapper
         mapper
       end
 
+      # @api private
       def source_aliases
         @connector.source_aliases
       end
 
+      # @api private
       def target_aliases
         @connector.target_aliases
       end
 
+      # @api private
       def mapper_name
         "#{@source_mapper.name}_X_#{Inflector.camelize(@connector.name)}_Mapper"
       end
 
+      # @api private
       def target_model_attribute_options
         {
           :collection => @connector.collection_target?,
