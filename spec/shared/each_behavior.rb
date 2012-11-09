f7757ed6eb149561e8e24b57fabaf69022b86953
snusnu <gamsnjaga@gmail.com>
2012-11-09 18:03:02 +0100

    Add shared_examples_for 'an #each method'

---
+++ spec/shared/each_behavior.rb
@@ -0,0 +1,13 @@
+shared_examples_for 'an #each method' do
+  it_should_behave_like 'a command method'
+
+  context 'with no block' do
+    subject { object.each }
+
+    it { should be_instance_of(to_enum.class) }
+
+    it 'yields the expected values' do
+      subject.to_a.should eql(object.to_a)
+    end
+  end
+end
