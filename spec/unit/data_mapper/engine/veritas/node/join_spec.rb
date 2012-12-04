a8709172143d7875c1d6fc774e64d343ceeef495
snusnu <gamsnjaga@gmail.com>
2012-12-04 19:05:58 +0100

    Add another expectation to Node#join unit spec

--- spec/unit/data_mapper/engine/veritas/node/join_spec.rb
+++ spec/unit/data_mapper/engine/veritas/node/join_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 # TODO make these specs more strict!
 describe Engine::Veritas::Node, '#join' do
-  subject { object.join(other) }
+  subject { object.join(other, join_definition) }
 
   let(:object)          { described_class.new(:users,   source_relation, source_aliases) }
   let(:other)           { described_class.new(:address, target_relation, target_aliases) }
@@ -10,11 +10,13 @@ describe Engine::Veritas::Node, '#join' do
   let(:target_relation) { mock('target_relation') }
   let(:join_relation)   { mock('join_relation') }
 
-  let(:source_aliases) { Relation::Graph::Node::Aliases::Unary.new({ :users_id     => :users_id     }, { :id => :users_id }) }
-  let(:target_aliases) { Relation::Graph::Node::Aliases::Unary.new({ :addresses_id => :addresses_id }, { :id => :addresses_id }) }
-  let(:joined_aliases) { source_aliases.join(target_aliases, {}) }
+  let(:source_aliases)  { Relation::Graph::Node::Aliases::Unary.new({ :users_id     => :users_id     }, { :id => :users_id }) }
+  let(:target_aliases)  { Relation::Graph::Node::Aliases::Unary.new({ :addresses_id => :addresses_id }, { :id => :addresses_id }) }
+  let(:joined_aliases)  { mock('joined_aliases') }
+  let(:join_definition) { {} }
 
   before do
+    source_aliases.should_receive(:join).with(target_aliases, join_definition).and_return(joined_aliases)
     source_relation.should_receive(:rename).with(joined_aliases).and_return(source_relation)
     source_relation.should_receive(:join).with(target_relation).and_return(join_relation)
   end
@@ -22,4 +24,5 @@ describe Engine::Veritas::Node, '#join' do
   it { should be_instance_of(described_class) }
 
   its(:relation) { should be(join_relation) }
+  its(:aliases)  { should be(joined_aliases) }
 end
