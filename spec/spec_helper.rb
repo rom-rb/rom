05068eb26b8a1b16f9c59e1d2c717ac0271022fd
Piotr Solnica <piotr.solnica@gmail.com>
2012-12-09 00:16:08 +0000

    Enable simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,18 @@ require 'backports'
 require 'backports/basic_object' unless defined?(BasicObject)
 require 'rubygems'
 
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter "/spec/"
+    add_group "Finalizer",    "lib/data_mapper/finalizer"
+    add_group "Mapper",       "lib/data_mapper/mapper"
+    add_group "Relation",     "lib/data_mapper/relation"
+    add_group "Relationship", "lib/data_mapper/relationship"
+    add_group "Engine",       "lib/data_mapper/engine"
+  end
+end
+
 require 'pp'
 require 'ostruct'
 require 'dm-mapper'
