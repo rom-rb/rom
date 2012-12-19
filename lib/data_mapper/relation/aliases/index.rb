3611fd5abceaf76cc5e7e7c98dd1f6c78d9cb2bd
snusnu <gamsnjaga@gmail.com>
2012-12-19 03:35:00 +0100

    Add docs for DataMapper::Relation::Aliases::Index

--- lib/data_mapper/relation/aliases/index.rb
+++ lib/data_mapper/relation/aliases/index.rb
@@ -6,9 +6,31 @@ module DataMapper
 
         include Equalizer.new(:entries)
 
+        # The entries managed by this instance
+        #
+        # @return [Hash<Attribute, Attribute>]
+        #
+        # @api private
         attr_reader :entries
+
+        # The header represented by this instance
+        #
+        # @return [Set<Attribute>]
+        #
+        # @api private
         attr_reader :header
 
+        # Initialize a new instance
+        #
+        # @param [Hash<Attribute, Attribute>] entries
+        #   the entries to manage
+        #
+        # @param [Strategy] strategy
+        #   the strategy to use for {#join}
+        #
+        # @return [undefined]
+        #
+        # @api private
         def initialize(entries, strategy)
           @entries  = entries
           @inverted = @entries.invert
@@ -16,14 +38,40 @@ module DataMapper
           @strategy = strategy.new(self)
         end
 
+        # Join this instance with another one
+        #
+        # @see Strategy#join
+        #
+        # @param [args]
+        #   the arguments accepted by {Strategy#join}
+        #
+        # @return [Index]
+        #
+        # @api private
         def join(*args)
           @strategy.join(*args)
         end
 
+        # Rename this instance
+        #
+        # @param [Hash<Symbol, Symbol>] aliases
+        #   the aliases to use for renaming
+        #
+        # @return [Index]
+        #
+        # @api private
         def rename(aliases)
           self.class.new(renamed_entries(aliases), @strategy.class)
         end
 
+        # The aliases needed to mimic +other+
+        #
+        # @param [Index] other
+        #   the other index to calculate aliases for
+        #
+        # @return [Hash<Symbol, Symbol>
+        #
+        # @api private
         def aliases(other)
           entries.each_with_object({}) { |(key, name), aliases|
             other_name = other[key].name
@@ -33,10 +81,31 @@ module DataMapper
           }
         end
 
+        # Return the current {Attribute} for the given +key+
+        #
+        # @param [Attribute] key
+        #   the original attribute
+        #
+        # @return [Attribute]
+        #   the current {Attribute} if the given +key+ was found
+        #
+        # @return [nil]
+        #   otherwise
+        #
+        # @api private
         def [](key)
           entries[key]
         end
 
+        # Tests wether this instance contains the given +field+
+        #
+        # @param [Symbol] field
+        #   the field to test for
+        #
+        # @return [true] if the instance contains the given +field+
+        # @return [false] otherwise
+        #
+        # @api private
         def field?(field)
           entries.values.any? { |name| name.field == field }
         end
