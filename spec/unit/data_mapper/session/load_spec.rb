cb2c467fcde684b4d4ab9e112b840eca361d8c70
Markus Schirp <mbj@seonic.net>
2013-01-01 13:55:13 +0100

    Simplify Session#load spec

--- spec/unit/data_mapper/session/load_spec.rb
+++ spec/unit/data_mapper/session/load_spec.rb
@@ -3,21 +3,14 @@ require 'spec_helper'
 describe DataMapper::Session, '#load' do
   subject { object.load(loader) }
 
-  let(:object)        { described_class.new(registry)              }
-  let(:mapper)        { registry.resolve_model(Spec::DomainObject) }
-  let(:registry)      { Spec::Registry.new                         }
-  let(:object)        { described_class.new(registry)              }
-  let(:identity)      { mapper.dumper(domain_object).identity      }
-  let(:domain_object) { Spec::DomainObject.new                     }
-
-  let(:loader) do
-    mock(
-      'Loader', 
-      :identity => identity, 
-      :object => domain_object, 
-      :mapper => mapper
-    )
-  end
+  let(:object)        { described_class.new(registry)                    }
+  let(:mapper)        { registry.resolve_model(Spec::DomainObject)       }
+  let(:registry)      { Spec::Registry.new                               }
+  let(:object)        { described_class.new(registry)                    }
+  let(:identity)      { mapper.dumper(domain_object).identity            }
+  let(:domain_object) { loader.object                                    }
+  let(:tuple)         { { :key_attribute => :a, :other_attribute => :b } }
+  let(:loader)        { mapper.loader(tuple)                             }
 
   context 'when object is not loaded before' do
 
