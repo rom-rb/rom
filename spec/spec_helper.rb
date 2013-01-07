f5e3335b38c3cce8b2ea11effc049b9f5af5b30d
snusnu <gamsnjaga@gmail.com>
2013-01-07 17:39:50 +0100

    Run all examples when everything is filtered

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,6 +41,7 @@ end
 RSpec.configure do |config|
 
   config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
 
   config.after(:each) do
     if example.metadata[:example_group][:file_path] =~ /unit|shared/
--- spec/spec_helper_integration.rb
+++ spec/spec_helper_integration.rb
@@ -8,6 +8,7 @@ require 'db_setup'
 RSpec.configure do |config|
 
   config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
 
   config.after(:all) do
     DM_ENV.reset
