a1b86214501a33a48e40cf1f94aed0b0d1204402
snusnu <gamsnjaga@gmail.com>
2013-01-14 22:01:47 +0100

    Add specs for Relation::Aliases::RelationIndex.build

---
+++ spec/unit/data_mapper/relation/aliases/relation_index/class_methods/build_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Relation::Aliases::RelationIndex, '.build' do
+  subject { described_class.build(attribute_index) }
+
+  let(:attribute_index) {
+    Relation::Aliases::AttributeIndex.build(relation_name, attribute_set, strategy_class)
+  }
+
+  let(:relation_name)  { :users }
+  let(:attribute_set)  { AttributeSet.new << mock_attribute(:id, Integer) }
+  let(:strategy_class) { Relation::Aliases::Strategy::NaturalJoin }
+
+  it { should eql(described_class.new({ :users => 1 })) }
+end
