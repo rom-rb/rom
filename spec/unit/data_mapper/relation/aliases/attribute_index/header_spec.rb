878e6108354446c4bb4671e0a806e60ce1a72e88
snusnu <gamsnjaga@gmail.com>
2013-01-14 23:23:27 +0100

    Add specs for Relation::Aliases::AttributeIndex#header

---
+++ spec/unit/data_mapper/relation/aliases/attribute_index/header_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe Relation::Aliases::AttributeIndex, '#header' do
+  subject { described_class.new(entries, strategy_class) }
+
+  let(:entries)        { { initial => current } }
+  let(:initial)        { attribute_alias(:initial_id, :users) }
+  let(:current)        { attribute_alias(:current_id, :users) }
+  let(:strategy_class) { mock }
+
+  its(:header) { should eql(Set[current]) }
+end
