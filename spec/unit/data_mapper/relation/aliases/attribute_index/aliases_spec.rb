281598e9713ec458e237430b5781f7d8e7b835c4
snusnu <gamsnjaga@gmail.com>
2013-01-15 01:03:51 +0100

    Add specs for Relation::Aliases::AttributeIndex#aliases

---
+++ spec/unit/data_mapper/relation/aliases/attribute_index/aliases_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Relation::Aliases::AttributeIndex, '#aliases' do
+  subject { object.aliases(other) }
+
+  let(:object) { described_class.new(entries, strategy_class) }
+
+  let(:entries)         { { initial       => current } }
+  let(:initial)         { attribute_alias(:initial_id, :users) }
+  let(:current)         { attribute_alias(:current_id, :users) }
+  let(:strategy_class)  { mock }
+
+  context "when self.eql(other)" do
+    let(:other) { object }
+
+    it { should eql({}) }
+  end
+
+  context "when !self.eql(other)" do
+    let(:other)         { described_class.new(other_entries, strategy_class) }
+    let(:other_entries) { { other_initial => other_current } }
+    let(:other_initial) { attribute_alias(:initial_id,       :users) }
+    let(:other_current) { attribute_alias(:other_current_id, :users) }
+
+    it { should eql(:current_id => :other_current_id) }
+  end
+end
