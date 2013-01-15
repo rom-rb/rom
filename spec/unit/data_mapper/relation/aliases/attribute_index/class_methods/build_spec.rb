90fe3fe9616e4aa0e87d759f7702f08d5ece8f2e
snusnu <gamsnjaga@gmail.com>
2013-01-15 01:07:13 +0100

    Fix specs for Relation::Aliases::AttributeIndex.build
    Kills the following mutation:
    
    @@ -1,5 +1,5 @@
     def self.initial_entries(relation_name, attribute_set)
    -  attribute_set.primitives.each_with_object({}) do |attribute, entries|
    +  attribute_set.each_with_object({}) do |attribute, entries|
         entry = Attribute.build(attribute.field, relation_name)
         entries[entry] = entry
       end

--- spec/unit/data_mapper/relation/aliases/attribute_index/class_methods/build_spec.rb
+++ spec/unit/data_mapper/relation/aliases/attribute_index/class_methods/build_spec.rb
@@ -3,10 +3,12 @@ require 'spec_helper'
 describe Relation::Aliases::AttributeIndex, '.build' do
   subject { described_class.build(relation_name, attribute_set, strategy_class) }
 
-  let(:relation_name)   { :users }
-  let(:attribute_set)   { AttributeSet.new << mock_attribute(:id, Integer) }
-  let(:strategy_class)  { mock }
-  let(:initial_entries) { { attribute_alias(:id, :users) => attribute_alias(:id, :users) } }
+  let(:relation_name)       { :users }
+  let(:attribute_set)       { AttributeSet.new << primitive_attribute << custom_attribute }
+  let(:primitive_attribute) { mock_attribute(:id,   Integer) }
+  let(:custom_attribute)    { mock_attribute(:name, mock)  }
+  let(:strategy_class)      { mock }
+  let(:initial_entries)     { { attribute_alias(:id, :users) => attribute_alias(:id, :users) } }
 
   it { should eql(described_class.new(initial_entries, strategy_class)) }
 end
