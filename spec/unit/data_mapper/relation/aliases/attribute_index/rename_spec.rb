caa83d6130d5f59017b78ae59356a9ebd8f2a29f
snusnu <gamsnjaga@gmail.com>
2013-01-15 18:48:26 +0100

    Fix specs for Relation::Aliases::AttributeIndex#rename
    Kills the following mutation:
    
    @@ -1,6 +1,6 @@
     def with_entries(name, filter)
       entries.each do |initial, current|
    -    if filter.call(name, current)
    +    if filter
           yield(initial, current)
         end
       end
     end

--- spec/unit/data_mapper/relation/aliases/attribute_index/rename_spec.rb
+++ spec/unit/data_mapper/relation/aliases/attribute_index/rename_spec.rb
@@ -8,13 +8,22 @@ describe Relation::Aliases::AttributeIndex, '#rename' do
   let(:entries) { { initial => current } }
   let(:initial) { attribute_alias(:initial_id, :users) }
   let(:current) { attribute_alias(:current_id, :users) }
-  let(:aliases) { { :current_id => renamed_current_id } }
 
   let(:strategy_class) { mock }
 
-  let(:renamed_entries)    { { initial => renamed_current } }
-  let(:renamed_current)    { attribute_alias(renamed_current_id, :users) }
-  let(:renamed_current_id) { :renamed_current_id }
+  context "when the field to rename is indexed" do
+    let(:aliases) { { :current_id => renamed_current_id } }
 
-  it { should eql(described_class.new(renamed_entries, strategy_class)) }
+    let(:renamed_entries)    { { initial => renamed_current } }
+    let(:renamed_current)    { attribute_alias(renamed_current_id, :users) }
+    let(:renamed_current_id) { :renamed_current_id }
+
+    it { should eql(described_class.new(renamed_entries, strategy_class)) }
+  end
+
+  context "when the field to rename is not indexed" do
+    let(:aliases) { { mock => :current_id } }
+
+    it { should eql(described_class.new(entries, strategy_class)) }
+  end
 end
