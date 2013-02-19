e87a184e52132655739c6ba68a7ffee7465edc64
snusnu <gamsnjaga@gmail.com>
2013-02-19 13:23:44 +0100

    Remove unused rspec :focus metadata configuration
    I actually remember that being kinda dangerous
    from app development. If you happen to forget to
    remove a :focus flag, only the focused tests will
    be run.
    
    During library development, I found myself simply
    running the specs in question explicitly, using
    something like:
    
      bundle exec rspec -cfs spec/file/path

--- spec/spec_helper_integration.rb
+++ spec/spec_helper_integration.rb
@@ -17,9 +17,6 @@ include(DataMapper)
 
 RSpec.configure do |config|
 
-  config.filter_run :focus => true
-  config.run_all_when_everything_filtered = true
-
   config.after(:all) do
     DM_ENV.reset
   end
