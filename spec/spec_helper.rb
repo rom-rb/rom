a3f662bb77f4a0d92b5f26865f38919af878a48d
snusnu <gamsnjaga@gmail.com>
2013-03-03 02:46:05 +0100

    Start simplecov before requiring dm-mapper
    This makes sure that simplecov actually can find
    the code under (coverage) test.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,24 @@
-require 'dm-mapper'
+# encoding: utf-8
 
-require 'shared_helper'
-
+# SimpleCov MUST be started before require 'dm-mapper'
+#
 if ENV['COVERAGE']
   require 'simplecov'
+
   SimpleCov.start do
-    add_filter "/spec/"
+    command_name 'spec:unit'
+
+    add_filter "spec"
+    add_filter "config"
+    add_filter "lib/data_mapper/support"
+
     add_group "Finalizer",    "lib/data_mapper/finalizer"
     add_group "Mapper",       "lib/data_mapper/mapper"
     add_group "Relation",     "lib/data_mapper/relation"
     add_group "Relationship", "lib/data_mapper/relationship"
-    add_group "Engine",       "lib/data_mapper/engine"
+    add_group "Attribute",    "lib/data_mapper/attribute"
   end
+
 end
+
+require 'shared_helper' # requires dm-mapper
