e19d3df30993635bda6630c4b11a20bc4b2e3c62
snusnu <gamsnjaga@gmail.com>
2013-03-07 18:09:15 +0100

    Move MissingMapperError next to StateError

--- lib/data_mapper/session/missing_mapper_error.rb
+++
@@ -1,8 +0,0 @@
-module DataMapper
-  class Session
-
-    # Error raised when session misses a mapper
-    class MissingMapperError < RuntimeError
-    end
-  end
-end
--- lib/dm-session.rb
+++ lib/dm-session.rb
@@ -6,8 +6,12 @@ require 'abstract_type'
 # Namespace module
 module DataMapper
   class Session
-    # Exception thrown on illegal state
+
+    # Error raised on illegal state
     class StateError < RuntimeError; end
+
+    # Error raised when session misses a mapper
+    class MissingMapperError < RuntimeError; end
   end
 end
 
@@ -16,4 +20,3 @@ require 'data_mapper/session/reader'
 require 'data_mapper/session/state'
 require 'data_mapper/session/operand'
 require 'data_mapper/session/registry'
-require 'data_mapper/session/missing_mapper_error'
