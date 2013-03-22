2acd3f1065d3d934e4c2d0b33fed03bd4b0d66c1
snusnu <gamsnjaga@gmail.com>
2013-03-22 17:46:13 +0100

    Add better specs for self referential many_to_many
    The new specs expose a bug in the current code and
    therefore one of the specs had to be marked pending
    for now.

--- spec/integration/self_referential/many_to_many_spec.rb
+++ spec/integration/self_referential/many_to_many_spec.rb
@@ -29,29 +29,35 @@ describe 'Relationship - Self referential Many To Many' do
     link_mapper.belongs_to :followed, person_model
   end
 
+  let(:john_with_followed_people)  { person_model.new(:id => 1, :name => 'John',  :parent_id => nil, :followed_people => [ jane ]) }
+  let(:jane_with_followed_people)  { person_model.new(:id => 2, :name => 'Jane',  :parent_id => nil, :followed_people => [ john ]) }
+  let(:alice_with_followed_people) { person_model.new(:id => 3, :name => 'Alice', :parent_id => nil, :followed_people => [ jane ]) }
+
+  let(:john_with_followers)        { person_model.new(:id => 1, :name => 'John',  :parent_id => nil, :followers => [ jane        ]) }
+  let(:jane_with_followers)        { person_model.new(:id => 2, :name => 'Jane',  :parent_id => nil, :followers => [ john, alice ]) }
+  let(:alice_with_followers)       { person_model.new(:id => 3, :name => 'Alice', :parent_id => nil, :followers => [             ]) }
+
+  let(:john)  { person_model.new(:id => 1, :name => 'John',  :parent_id => nil ) }
+  let(:jane)  { person_model.new(:id => 2, :name => 'Jane',  :parent_id => nil ) }
+  let(:alice) { person_model.new(:id => 3, :name => 'Alice', :parent_id => nil ) }
+
   it 'loads all followed people' do
     pending if RUBY_VERSION < '1.9'
 
-    people = DM_ENV[person_model].include(:followed_people).to_a
-
-    john = people[0]
-    jane = people[1]
-
-    john.followed_people.count.should == 1
-    john.followed_people.first.id.should eql(jane.id)
-    john.followed_people.first.name.should eql(jane.name)
+    DM_ENV[person_model].include(:followed_people).to_a.should =~ [
+      john_with_followed_people,
+      jane_with_followed_people,
+      alice_with_followed_people
+    ]
   end
 
   it 'loads all followers' do
-    pending if RUBY_VERSION < '1.9'
+    pending "Loading a person's followers doesn't work yet"
 
-    people = DM_ENV[person_model].include(:followers).to_a
-
-    john = people[0]
-    jane = people[1]
-
-    john.followers.count.should == 1
-    john.followers.first.id.should eql(jane.id)
-    john.followers.first.name.should eql(jane.name)
+    DM_ENV[person_model].include(:followers).to_a.should =~ [
+      john_with_followers,
+      jane_with_followers,
+      alice_with_followers
+    ]
   end
 end
