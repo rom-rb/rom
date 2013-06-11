f70326976e3bfd7e49a48da55028db63476334ef
Piotr Solnica <piotr.solnica@gmail.com>
2013-06-11 12:36:13 +0200

    Set OpenStruct as default model

--- spec/unit/rom/mapper/dump_spec.rb
+++ spec/unit/rom/mapper/dump_spec.rb
@@ -1,13 +1,12 @@
 require 'spec_helper'
 
 describe Mapper, '#dump' do
-  subject(:mapper) { described_class.new(header, model) }
+  subject(:mapper) { described_class.new(header) }
 
-  let(:model)  { OpenStruct }
   let(:header) { Axiom::Relation::Header.coerce([[:id, Integer ], [:name, String]]) }
   let(:tuple)  { Hash[id: 1, name: 'Jane'] }
   let(:data)   { [1, 'Jane'] }
-  let(:object) { model.new(tuple) }
+  let(:object) { OpenStruct.new(tuple) }
 
   it 'dumps the object into tuple data' do
     expect(mapper.dump(object)).to eq(data)
