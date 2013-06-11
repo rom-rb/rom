78d4184af3f77203ec1ef21bd374d8384c292d6d
Piotr Solnica <piotr.solnica@gmail.com>
2013-06-11 08:43:52 +0200

    Rename axiom_relation => relation

--- lib/rom/relation.rb
+++ lib/rom/relation.rb
@@ -6,45 +6,45 @@ module ROM
   class Relation
     include Enumerable
 
-    attr_reader :axiom_relation, :mapper
+    attr_reader :relation, :mapper
 
     alias_method :all, :to_a
 
-    def initialize(axiom_relation, mapper)
-      @axiom_relation = axiom_relation
-      @mapper         = mapper
+    def initialize(relation, mapper)
+      @relation = relation
+      @mapper   = mapper
     end
 
     def each(&block)
       return to_enum unless block_given?
-      axiom_relation.each { |tuple| yield(mapper.load(tuple)) }
+      relation.each { |tuple| yield(mapper.load(tuple)) }
       self
     end
 
     def insert(object)
-      new(axiom_relation.insert(mapper.dump(object)))
+      new(relation.insert(mapper.dump(object)))
     end
     alias_method :<<, :insert
 
     def update(object)
       tuple = mapper.dump(object)
-      new(axiom_relation.delete(tuple).insert(tuple))
+      new(relation.delete(tuple).insert(tuple))
     end
 
     def delete(object)
-      new(axiom_relation.delete(mapper.dump(object)))
+      new(relation.delete(mapper.dump(object)))
     end
 
     def replace(objects)
-      new(axiom_relation.replace(objects.map { |object| mapper.dump(object).flatten }))
+      new(relation.replace(objects.map { |object| mapper.dump(object).flatten }))
     end
 
     def restrict(*args, &block)
-      new(axiom_relation.restrict(*args, &block))
+      new(relation.restrict(*args, &block))
     end
 
     def take(limit)
-      new(axiom_relation.take(limit))
+      new(relation.take(limit))
     end
 
     def first(limit = 1)
@@ -52,30 +52,30 @@ module ROM
     end
 
     def last(limit = 1)
-      new(axiom_relation.reverse.take(limit).reverse)
+      new(relation.reverse.take(limit).reverse)
     end
 
     def drop(offset)
-      new(axiom_relation.drop(offset))
+      new(relation.drop(offset))
     end
 
     def order(*attributes)
-      sorted = axiom_relation.sort_by { |r| attributes.map { |attribute| r.send(attribute) } }
+      sorted = relation.sort_by { |r| attributes.map { |attribute| r.send(attribute) } }
       new(sorted)
     end
 
     def sort_by(*args, &block)
-      new(axiom_relation.sort_by(*args, &block))
+      new(relation.sort_by(*args, &block))
     end
 
     def ordered
-      new(axiom_relation.sort_by(axiom_relation.header))
+      new(relation.sort_by(relation.header))
     end
 
     private
 
-    def new(new_axiom_relation)
-      self.class.new(new_axiom_relation, mapper)
+    def new(new_relation)
+      self.class.new(new_relation, mapper)
     end
 
   end # class Relation
