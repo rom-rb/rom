6e772756f3fa9aaca3b88fb91522f9879e931124
Piotr Solnica <piotr.solnica@gmail.com>
2013-06-14 13:34:21 +0200

    [WIP] Introduce AttributeSet

--- lib/rom/mapper/header.rb
+++ lib/rom/mapper/header.rb
@@ -2,30 +2,45 @@ module ROM
   class Mapper
     Attribute = Struct.new(:field, :name)
 
-    class Header
-      include Enumerable, Concord.new(:header, :mapping)
+    class AttributeSet
+      include Enumerable, Concord.new(:attributes)
 
-      def self.coerce(attributes, options = {})
-        header  = Axiom::Relation::Header.coerce(attributes)
+      def self.coerce(header, mapping = {})
+        merged_mapping = Hash[
+          header.map { |attribute| [ attribute.name, attribute.name ] }
+        ].merge(mapping)
 
-        # FIXME: hide this complexity behind an attribute set object
-        mapping = Hash[header.map { |attribute| [ attribute.name, attribute.name ] }]
-        mapping.update(options.fetch(:map, {}))
-
-        attributes = mapping.each_with_object({}) { |(field, name), object|
-          object[field] = Attribute.new(field, name)
+        attributes = merged_mapping.each_with_object({}) { |(field, name), object|
+          object[name] = Attribute.new(field, name)
         }
 
+        new(attributes)
+      end
+
+      def each(&block)
+        return to_enum unless block_given?
+        attributes.each_value(&block)
+        self
+      end
+
+      def [](name)
+        attributes[name]
+      end
+
+    end # AttributeSet
+
+    class Header
+      include Enumerable, Concord.new(:header, :attributes)
+
+      def self.coerce(attributes, options = {})
+        header     = Axiom::Relation::Header.coerce(attributes)
+        attributes = AttributeSet.coerce(header, options.fetch(:map, {}))
         new(header, attributes)
       end
 
       def each(&block)
         return to_enum unless block_given?
-
-        header.each do |attribute|
-          yield(mapping[attribute.name])
-        end
-
+        attributes.each(&block)
         self
       end
 
