adaf580a0c4d96afe81ca3b43aa99e15815fb58e
Piotr Solnica <piotr.solnica@gmail.com>
2013-06-14 12:16:35 +0200

    Move Header to Mapper namespace

---
+++ lib/rom/mapper/header.rb
@@ -0,0 +1,24 @@
+module ROM
+  class Mapper
+
+    class Header
+      include Enumerable, Concord.new(:header, :mapping)
+
+      def self.coerce(attributes, mapping = {})
+        new(Axiom::Relation::Header.coerce(attributes), mapping)
+      end
+
+      def each(&block)
+        return to_enum unless block_given?
+
+        header.each do |attribute|
+          yield(attribute, mapping.fetch(attribute.name) { attribute.name })
+        end
+
+        self
+      end
+
+    end # Header
+
+  end # Mapper
+end # ROM
