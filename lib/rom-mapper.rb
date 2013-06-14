f8f3fd4461d32569b745d40cf32328e2099f4fa7
Piotr Solnica <piotr.solnica@gmail.com>
2013-06-14 15:06:24 +0200

    Move attribute and attribute set into their own files

--- lib/rom-mapper.rb
+++ lib/rom-mapper.rb
@@ -3,5 +3,7 @@ require 'adamantium'
 require 'equalizer'
 require 'abstract_type'
 
+require 'rom/mapper/attribute'
+require 'rom/mapper/attribute_set'
 require 'rom/mapper/header'
 require 'rom/mapper'
