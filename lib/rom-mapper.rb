68324c84eda29f31618189fd76a102f46c490b64
Piotr Solnica <piotr.solnica@gmail.com>
2013-06-14 11:34:01 +0200

    Move mapper to its own file

--- lib/rom-mapper.rb
+++ lib/rom-mapper.rb
@@ -3,27 +3,4 @@ require 'adamantium'
 require 'equalizer'
 require 'abstract_type'
 
-module ROM
-
-  class Mapper
-    include Concord.new(:header, :model)
-
-    def self.new(header, model = OpenStruct)
-      super
-    end
-
-    def load(tuple)
-      model.new(
-        Hash[header.map { |attribute| [ attribute.name, tuple[attribute.name] ] }]
-      )
-    end
-
-    def dump(object)
-      header.each_with_object([]) { |attribute, tuple|
-        tuple << object.send(attribute.name)
-      }
-    end
-
-  end # Mapper
-
-end # ROM
+require 'rom/mapper'
---
+++ lib/rom/mapper.rb
@@ -0,0 +1,24 @@
+module ROM
+
+  class Mapper
+    include Concord.new(:header, :model)
+
+    def self.new(header, model = OpenStruct)
+      super
+    end
+
+    def load(tuple)
+      model.new(
+        Hash[header.map { |attribute| [ attribute.name, tuple[attribute.name] ] }]
+      )
+    end
+
+    def dump(object)
+      header.each_with_object([]) { |attribute, tuple|
+        tuple << object.send(attribute.name)
+      }
+    end
+
+  end # Mapper
+
+end # ROM
