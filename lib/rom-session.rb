c958b4f1ba3b5dfc96b007574eb26f7b8628e7ab
Piotr Solnica <piotr.solnica@gmail.com>
2013-06-20 16:12:48 +0200

    Nuke the code for now

--- lib/rom-session.rb
+++ lib/rom-session.rb
@@ -1,29 +1,13 @@
-require 'backports'
 require 'adamantium'
 require 'equalizer'
 require 'abstract_type'
 require 'concord'
 
-require 'rom-relation'
-require 'rom-mapper'
-
-# Namespace module
 module ROM
 
   # Session namespace
-  class Session
-
-    # Error raised on illegal state
-    class StateError < RuntimeError; end
-
-    # Error raised when session misses a mapper
-    class MissingMapperError < RuntimeError; end
+  module Session
 
   end # Session
-end # ROM
 
-require 'rom/session'
-require 'rom/session/reader'
-require 'rom/session/state'
-require 'rom/session/operand'
-require 'rom/session/registry'
+end # ROM
