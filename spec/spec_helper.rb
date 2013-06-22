1296610532f1c0b4ba794ec65f204f1ca83f649b
Piotr Solnica <piotr.solnica@gmail.com>
2013-06-22 20:12:51 +0200

    Add SimpleCov setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,21 @@
+if ENV['COVERAGE'] == 'true'
+  require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+
+  SimpleCov.start do
+    command_name 'spec:unit'
+
+    add_filter 'config'
+    add_filter 'lib/rom/support'
+    add_filter 'spec'
+  end
+end
+
 require 'devtools'
 
 Devtools.init_spec_helper
