f13577e5704b1166fa3d38890baf3aa1efaba39c
Piotr Solnica <piotr.solnica@gmail.com>
2013-06-24 23:48:41 +0200

    Add specs for Session::Relation#tracking?

---
+++ spec/unit/rom/session/relation/tracking_predicate_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Session::Relation, '#tracking?' do
+  subject { users.tracking?(user) }
+
+  include_context 'Session::Relation'
+
+  let(:user) { model.new(:id => 3, :name => 'John') }
+
+  context 'when the object is being tracked' do
+    before do
+      users.track(user)
+    end
+
+    it { should be(true) }
+  end
+
+  context 'when the object is not being tracked' do
+    it { should be(false) }
+  end
+end
