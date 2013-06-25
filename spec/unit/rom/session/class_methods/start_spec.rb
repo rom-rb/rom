45c150f873f531364fa658c7d6f6bee984d2cac1
Piotr Solnica <piotr.solnica@gmail.com>
2013-06-25 21:22:01 +0000

    Add spec for Session.start

---
+++ spec/unit/rom/session/class_methods/start_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe Session, '.start' do
+  include_context 'Session::Relation'
+
+  it 'starts a new session' do
+    Session.start(:users => relation) do |session|
+      expect(session).to be_instance_of(Session)
+      expect(session[:users]).to be_instance_of(Session::Relation)
+    end
+  end
+end
