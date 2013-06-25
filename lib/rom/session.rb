c698a84006050aec8e180636e659a85f709fa8b3
Piotr Solnica <piotr.solnica@gmail.com>
2013-06-25 21:10:31 +0000

    Add docs for Session

--- lib/rom/session.rb
+++ lib/rom/session.rb
@@ -1,18 +1,49 @@
 module ROM
 
+  # Extends ROM's environment with IdentityMap and state-tracking functionality
+  #
+  # @api public
   class Session
     include Concord.new(:environment)
 
+    # Start a new session
+    #
+    # @example
+    #
+    #   ROM::Session.start(env) do |session|
+    #     user = session[:users].new(name: 'Jane')
+    #     session[:users].save(user)
+    #     session[:users].flush
+    #   end
+    #
+    # @param [ROM::Environment] rom's environment
+    #
+    # @yieldparam [Session::Environment]
+    #
+    # @api public
     def self.start(environment, &block)
       yield(new(Environment.new(environment, Tracker.new)))
     end
 
+    # Return a session relation identified by name
+    #
+    # @param [Symbol] relation name
+    #
+    # @return [Session::Relation]
+    #
+    # @api public
     def [](relation_name)
       environment[relation_name]
     end
 
+    # Flush this session committing all the state changes
+    #
+    # @return [Session]
+    #
+    # @api public
     def flush
       environment.commit
+      self
     end
 
   end # Session
