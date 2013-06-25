f2cbb8287aa710c2c7e78b727c800ebb440d72d2
Piotr Solnica <piotr.solnica@gmail.com>
2013-06-25 21:31:10 +0000

    Add specs for Session#flush

---
+++ spec/unit/rom/session/flush_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe Session, '#flush' do
+  subject { session.flush }
+
+  include_context 'Session::Relation'
+
+  let(:john)  { session[:users].all.first }
+  let(:jane)  { session[:users].all.last }
+  let(:piotr) { session[:users].new(:id => 3, :name => 'Piotr') }
+
+  before do
+    session[:users].delete(john)
+
+    jane.name = 'Jane Doe'
+    session[:users].save(jane)
+
+    session[:users].save(piotr)
+  end
+
+  it 'commits all deletes' do
+    expect(subject[:users].state(john).relation.all).to_not include(john)
+  end
+
+  it 'commits all updates' do
+    expect(subject[:users].state(jane).relation.all.last).to eq(jane)
+  end
+
+  it 'commits all inserts' do
+    expect(subject[:users].state(piotr).relation.all).to include(piotr)
+  end
+end
