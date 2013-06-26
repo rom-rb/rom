8dc9b71cf89d3802c9e92149b50e00b6058a6dd0
Piotr Solnica <piotr.solnica@gmail.com>
2013-06-26 12:21:25 +0000

    Refactor spec for Session::Mapper#load

--- spec/unit/rom/session/mapper/load_spec.rb
+++ spec/unit/rom/session/mapper/load_spec.rb
@@ -20,32 +20,26 @@ describe Session::Mapper, '#load' do
     stub(loader).identity(tuple) { 1 }
   end
 
+  context 'when IM does not include the loaded object' do
+    it 'loads the object' do
+      stub(mapper).load(tuple) { user }
+      expect(subject).to be(user)
+      mapper.should have_received.load(tuple)
+    end
+
+    it 'stores persisted state in the tracker' do
+      expect(tracker.fetch(subject)).to be_persisted
+    end
+  end
+
   context 'when IM includes the loaded object' do
     before do
       im.store(1, user, tuple)
     end
 
-    after do
+    it 'returns already loaded object' do
+      expect(subject).to be(user)
       mapper.should_not have_received.load(tuple)
-    end
-
-    it { should be(user) }
-  end
-
-  context 'when IM does not include the loaded object' do
-    before do
-      stub(mapper).load(tuple) { user }
-    end
-
-    after do
-      mapper.should have_received.load(tuple)
-    end
-
-    it { should be(user) }
-
-    it 'stores persisted state in the tracker' do
-      subject
-      expect(tracker.fetch(user)).to be_persisted
     end
   end
 end
