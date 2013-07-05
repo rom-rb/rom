40421d943afb9c7fdd35bff186eff810794e37ff
Piotr Solnica <piotr.solnica@gmail.com>
2013-07-05 05:17:31 -0400

    Add shared examples for Loader#identity and Loader#call
    refs #2

--- spec/unit/rom/mapper/loader/object_builder/call_spec.rb
+++ spec/unit/rom/mapper/loader/object_builder/call_spec.rb
@@ -1,13 +1,5 @@
 require 'spec_helper'
 
 describe Mapper::Loader::ObjectBuilder, '#call' do
-  subject(:loader) { described_class.new(header, model) }
-
-  let(:header) { Mapper::Header.coerce([[:uid, Integer ], [:name, String]], :map => { :uid => :id }, :keys => [ :uid ]) }
-  let(:tuple)  { Hash[uid: 1, name: 'Jane'] }
-  let(:model)  { mock_model(:id, :name) }
-
-  it 'returns loaded object' do
-    expect(loader.call(tuple)).to eq(model.new(id: 1, name: 'Jane'))
-  end
+  it_behaves_like 'Mapper::Loader#call'
 end
