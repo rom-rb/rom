cbfe1709e6ae52bea5490ea4ea083dc989f7517e
Piotr Solnica <piotr.solnica@gmail.com>
2013-07-19 02:09:32 -0700

    Make spec pass

--- spec/integration/working_with_relations_spec.rb
+++ spec/integration/working_with_relations_spec.rb
@@ -1,24 +1,20 @@
 require 'spec_helper'
 
 describe 'Working with relations' do
-  let(:mapper_class) { Mapper }
-  let(:user_model)   { Class.new(OpenStruct) }
+  let(:header) { Axiom::Relation::Header.coerce([[:id, Integer], [:name, String]]) }
+  let(:mapper) { TestMapper.new(header, model) }
+  let(:model)  { Class.new(OpenStruct) }
 
   specify 'relation setup' do
-    pending 'in memory adapter is not finished yet'
-
     env  = ROM::Environment.coerce(:test => 'memory://test')
     repo = env.repository(:test)
 
-    repo.register(:users, [[:id, Integer], [:name, String]], :keys => [:id])
+    repo.register(Axiom::Relation::Base.new(:users, header))
 
-    users    = repo.get(:users)
-    mapper   = mapper_class.new(users)
-    relation = ROM::Relation.new(users, mapper)
+    users = ROM::Relation.new(repo.get(:users), mapper)
 
-    user = user_model.new(id: 1, name: 'Jane')
-    relation.insert(user)
+    user = model.new(id: 1, name: 'Jane')
 
-    expect(relation.all).to include(user)
+    expect(users.insert(user).all).to include(user)
   end
 end
