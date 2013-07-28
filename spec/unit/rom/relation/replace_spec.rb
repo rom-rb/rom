045747138a52918a363a5363f52965882a43b75c
Piotr Solnica <piotr.solnica@gmail.com>
2013-07-28 12:37:49 -0700

    DRY up spec

--- spec/unit/rom/relation/replace_spec.rb
+++ spec/unit/rom/relation/replace_spec.rb
@@ -3,13 +3,9 @@ require 'spec_helper'
 describe Relation, '#replace' do
   subject(:relation) { described_class.new(users, mapper) }
 
-  let(:users)  { Axiom::Relation.new([[ :name, String ]], [[ 'John' ], [ 'Jane' ]]) }
-  let(:model)  { mock_model(:name) }
-  let(:user1)  { model.new(name: 'John') }
-  let(:user2)  { model.new(name: 'Jane') }
-  let(:mapper) { TestMapper.new(users.header, model) }
+  include_context 'Relation'
 
   it 'replaces the relation with a new one' do
-    expect(relation.replace([ user2 ]).all).to eq([ user2 ])
+    expect(relation.replace([user2]).all).to eq([user2])
   end
 end
