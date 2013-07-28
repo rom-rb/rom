fbb3a07d7420ddb778b759ac6d5913cc9993d938
Piotr Solnica <piotr.solnica@gmail.com>
2013-07-28 12:29:49 -0700

    DRY up spec

--- spec/unit/rom/relation/delete_spec.rb
+++ spec/unit/rom/relation/delete_spec.rb
@@ -3,12 +3,9 @@ require 'spec_helper'
 describe Relation, '#delete' do
   subject(:relation) { described_class.new(users, mapper) }
 
-  let(:users)  { Axiom::Relation.new([[ :name, String ]], [[ 'John' ]]) }
-  let(:model)  { mock_model(:name) }
-  let(:user)   { model.new(name: 'John') }
-  let(:mapper) { TestMapper.new(users.header, model) }
+  include_context 'Relation'
 
   it 'deletes tuples from the relation' do
-    expect(relation.delete(user).all).to be_empty
+    expect(relation.delete(user).all).not_to include(user)
   end
 end
