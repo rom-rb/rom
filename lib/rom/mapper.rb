f78344cd443020c374d526cf703caa900f28243a
Piotr Solnica <piotr.solnica@gmail.com>
2013-07-30 10:39:27 -0700

    Add Mapper.build

--- lib/rom/mapper.rb
+++ lib/rom/mapper.rb
@@ -3,6 +3,19 @@ module ROM
   class Mapper
     include Concord::Public.new(:header, :loader, :dumper)
 
+    DEFAULT_LOADER = Loader::Allocator
+    DEFAULT_DUMPER = Dumper
+
+    def self.build(header, model, options = {})
+      loader_class = options.fetch(:loader_class) { DEFAULT_LOADER }
+      dumper_class = options.fetch(:dumper_class) { DEFAULT_DUMPER }
+
+      loader = loader_class.new(header, model)
+      dumper = dumper_class.new(header, model)
+
+      new(header, loader, dumper)
+    end
+
     def call(relation)
       relation.rename(header.mapping)
     end
--- spec/unit/rom/mapper/call_spec.rb
+++ spec/unit/rom/mapper/call_spec.rb
@@ -4,19 +4,20 @@ describe Mapper, '#call' do
   subject { object.call(relation) }
 
   let(:object) {
-    Mapper.new(header, loader, dumper)
+    Mapper.build(header, model)
   }
 
   let(:header) {
     Mapper::Header.coerce(relation.header, map: { user_id: :id, user_name: :name })
   }
 
+  let(:model) {
+    OpenStruct
+  }
+
   let(:relation) {
     Axiom::Relation::Base.new(:users, [[:user_id, Integer], [:user_name, String]])
   }
-
-  fake(:loader) { Mapper::Loader }
-  fake(:dumper) { Mapper::Dumper }
 
   it 'renames relation' do
     expect(subject.header.map(&:name)).to eq([:id, :name])
