a6eb76a3d9d11b1b43ee3c9e7eb49fef466c8ca3
Piotr Solnica <piotr.solnica@gmail.com>
2013-07-31 09:04:26 -0700

    Remove redundant code

--- lib/rom/mapping.rb
+++ lib/rom/mapping.rb
@@ -4,63 +4,6 @@ module ROM
 
   class Mapping
 
-    class Definition
-
-      def self.build(header, &block)
-        new(header, &block).freeze
-      end
-
-      def initialize(header, &block)
-        @header     = header
-        @map        = {}
-        @attributes = []
-        @mapper     = nil
-        instance_eval(&block)
-      end
-
-      def header
-        Mapper::Header.build(project_header, map: mapping)
-      end
-
-      def mapper(mapper = Undefined)
-        if mapper == Undefined
-          @mapper
-        else
-          @mapper = mapper
-        end
-      end
-
-      def mapping
-        @map
-      end
-
-      def model(model = Undefined)
-        if model == Undefined
-          @model
-        else
-          @model = model
-        end
-      end
-
-      def map(*args)
-        options = args.last
-
-        if options.is_a?(Hash)
-          @map.update(args.first => options[:to])
-        else
-          @attributes.concat(args)
-        end
-
-        self
-      end
-
-      private
-
-      def project_header
-        @header.project(@attributes.concat(@map.keys))
-      end
-    end
-
     attr_reader :env, :registry, :model
 
     # @api public
