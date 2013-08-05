2b66939a81c2dd071ad3aa31887ee5c02c25eeb6
Piotr Solnica <piotr.solnica@gmail.com>
2013-08-05 23:30:51 +0000

    Simplify spec setup a bit

--- spec/shared/unit/environment_context.rb
+++ spec/shared/unit/environment_context.rb
@@ -3,7 +3,7 @@ shared_context 'Session::Environment' do
 
   let(:users) {
     relation = TEST_ENV.repository(:test)[:users]
-    mapper   = Mapper.build(Mapper::Header.build(relation.header), mock_model(:id, :name))
+    mapper   = Mapper.build(relation.header, mock_model(:id, :name))
     Relation.new(relation, mapper)
   }
 end
--- spec/shared/unit/relation_context.rb
+++ spec/shared/unit/relation_context.rb
@@ -6,7 +6,7 @@ shared_context 'Session::Relation' do
   let(:env)      { Session::Environment.new({ :users => relation }, tracker) }
   let(:tracker)  { Session::Tracker.new }
 
-  let(:mapper)   { Mapper.build(Mapper::Header.build([[:id, Integer], [:name, String]], :keys => [:id]), model) }
+  let(:mapper)   { Mapper.build([[:id, Integer], [:name, String]], model, :keys => [:id]) }
   let(:model)    { mock_model(:id, :name) }
   let(:header)   { TEST_ENV.schema[:users].header }
   let(:axiom)    { Axiom::Relation::Variable.new(Axiom::Relation.new(header, [[1, 'John'], [2, 'Jane']])) }
