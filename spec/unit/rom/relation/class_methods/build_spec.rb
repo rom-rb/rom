9d0596142d6cfc5e4cf54b96f49b21c9fbeffc49
Piotr Solnica <piotr.solnica@gmail.com>
2013-08-30 13:21:19 +0200

    Kill mutation in Relation.build (:heart: mutant)

--- spec/unit/rom/relation/class_methods/build_spec.rb
+++ spec/unit/rom/relation/class_methods/build_spec.rb
@@ -5,15 +5,16 @@ require 'spec_helper'
 describe Relation, '.build' do
   subject { described_class.build(relation, mapper) }
 
-  fake(:relation)        { Axiom::Relation }
-  fake(:mapped_relation) { Axiom::Relation }
-  fake(:mapper)          { Mapper }
+  fake(:relation)  { Axiom::Relation }
+  fake(:mapped)    { Axiom::Relation }
+  fake(:optimized) { Axiom::Relation }
+  fake(:mapper)    { Mapper }
 
   before do
-    stub(mapper).call(relation)    { mapped_relation }
-    stub(mapped_relation).optimize { mapped_relation }
+    stub(mapper).call(relation) { mapped }
+    stub(mapped).optimize       { optimized }
   end
 
-  its(:relation) { should be(mapped_relation) }
+  its(:relation) { should be(optimized) }
   its(:mapper)   { should be(mapper) }
 end
