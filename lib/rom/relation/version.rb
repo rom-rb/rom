3c37a63ad8bfced297cdcf603a44953592729bb2
Piotr Solnica <piotr.solnica@gmail.com>
2013-09-02 10:45:17 +0200

    Move version file

---
+++ lib/rom/relation/version.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+module ROM
+  class Relation
+
+    VERSION = '0.1.2'.freeze
+
+  end # Relation
+end # Relation
--- lib/rom/version.rb
+++
@@ -1,7 +0,0 @@
-# encoding: utf-8
-
-module ROM
-  class Relation
-    VERSION = '0.1.2'
-  end
-end
