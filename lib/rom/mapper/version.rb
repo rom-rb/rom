d39e55490fea5ed737ce975c827007807385cb13
Piotr Solnica <piotr.solnica@gmail.com>
2014-03-21 17:17:30 +0100

    Remove obsolete version files

--- lib/rom/mapper/version.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-module ROM
-  class Mapper
-
-    VERSION = '0.1.1'.freeze
-
-  end # Mapper
-end # ROM
--- lib/rom/relation/version.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-module ROM
-  class Relation
-
-    VERSION = '0.1.2'.freeze
-
-  end # Relation
-end # Relation
