c559a9aedc920de303a151e6643f4a6d7935a4f0
Piotr Solnica <piotr.solnica@gmail.com>
2014-10-15 16:03:22 +0200

    Remove old specs

--- spec/unit/rom/mapper/attribute/rename_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper"
-
-describe Mapper::Attribute, '#rename' do
-  subject(:attribute) { Mapper::Attribute.build(:title) }
-
-  it 'returns a new attribute with changed name' do
-    expect(attribute.rename(:book_title)).to eql(Mapper::Attribute.build(:book_title))
-  end
-end
