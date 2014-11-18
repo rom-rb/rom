278ac01bce8d864a2493d97200192644b744c18d
Piotr Solnica <piotr.solnica@gmail.com>
2014-11-18 16:54:54 +0100

    Fix spec description

--- spec/integration/ra/wrap_spec.rb
+++ spec/integration/ra/wrap_spec.rb
@@ -5,7 +5,7 @@ describe 'Wrap operation' do
 
   subject(:rom) { setup.finalize }
 
-  specify 'defining a grouped relation' do
+  specify 'defining a wrapped relation' do
     setup.relation(:users) do
       def with_task
         in_memory { wrap(natural_join(tasks), task: [:title, :priority]) }
