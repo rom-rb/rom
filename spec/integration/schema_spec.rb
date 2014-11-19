40a8074ae636a393261ae7af53f7173c982ec7ee
Piotr Solnica <piotr.solnica@gmail.com>
2014-11-19 19:12:27 +0100

    Update schema spec

--- spec/integration/schema_spec.rb
+++ spec/integration/schema_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe 'Defining schema' do
   let(:setup) { ROM.setup(memory: 'memory://localhost') }
+  let(:rom) { setup.finalize }
+  let(:schema) { rom.schema }
 
   describe '.schema' do
     it 'returns schema with relations' do
@@ -14,12 +16,14 @@ describe 'Defining schema' do
         end
       end
 
-      rom = setup.finalize
-      schema = rom.schema
       users = schema.users
 
       expect(users.dataset.to_a).to eql(rom.memory.users.to_a)
       expect(users.header).to eql([:id, :name])
+    end
+
+    it 'returns an empty schema if it was not defined' do
+      expect(schema.users).to be_nil
     end
   end
 end
