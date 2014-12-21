2e1576894fd3309737e9ba91a507a69e5e509166
Piotr Solnica <piotr.solnica@gmail.com>
2014-12-21 12:05:13 +0100

    Small refactor in MapperRegistry

--- lib/rom/mapper_registry.rb
+++ lib/rom/mapper_registry.rb
@@ -1,18 +1,22 @@
 module ROM
   # @private
   class MapperRegistry < Registry
+
+    # @api private
     def []=(name, mapper)
       elements[name] = mapper
     end
 
-    def key?(name)
-      elements.key?(name)
+    # @api private
+    def by_path(path)
+      elements[paths(path).detect { |name| elements.key?(name) }]
     end
 
-    def by_path(path)
-      names = path.split('.').map(&:to_sym)
-      mapper_key = names.reverse.detect { |name| key?(name) }
-      elements[mapper_key]
+    private
+
+    # @api private
+    def paths(path)
+      path.split('.').map(&:to_sym).reverse
     end
   end
 end
