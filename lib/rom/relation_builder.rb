fc0d263bb65a886cafa9a016cb6ca91fd48d765b
Piotr Solnica <piotr.solnica@gmail.com>
2015-01-03 02:09:17 +0100

    Use ClassBuilder in RelationBuilder

--- lib/rom/relation_builder.rb
+++ lib/rom/relation_builder.rb
@@ -17,46 +17,39 @@ module ROM
     # @api private
     def call(name)
       schema_relation = schema[name]
+      klass_name = "#{Relation.name}[#{Inflecto.camelize(name)}]"
 
-      klass = Class.new(Relation)
-
-      klass.class_eval <<-RUBY, __FILE__, __LINE__ + 1
-        def self.name
-          "#{Relation.name}[#{Inflecto.camelize(name)}]"
-        end
-
-        def self.inspect
-          name
-        end
-
-        def self.to_s
-          name
-        end
-
-        def name
-          #{name.inspect}
-        end
-
-        def respond_to_missing?(name, _include_private = false)
-          __relations__.key?(name) || super
-        end
-
-        private
-
-        def method_missing(name, *args, &block)
-          if __relations__.key?(name)
-            __relations__[name]
-          else
-            super
-          end
-        end
-      RUBY
-
+      klass = build_class(name, klass_name)
       klass.send(:include, mod)
 
       yield(klass)
 
       klass.new(schema_relation.dataset, schema_relation.header)
+    end
+
+    # @api private
+    def build_class(name, klass_name)
+      ClassBuilder.new(name: klass_name, parent: Relation).call do |klass|
+        klass.class_eval <<-RUBY, __FILE__, __LINE__ + 1
+          def name
+            #{name.inspect}
+          end
+
+          def respond_to_missing?(name, _include_private = false)
+            __relations__.key?(name) || super
+          end
+
+          private
+
+          def method_missing(name, *args, &block)
+            if __relations__.key?(name)
+              __relations__[name]
+            else
+              super
+            end
+          end
+        RUBY
+      end
     end
   end
 end
