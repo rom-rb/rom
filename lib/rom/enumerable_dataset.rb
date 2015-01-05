a2ce63815425afa001cd1f924151c50d8d216ad5
Piotr Solnica <piotr.solnica@gmail.com>
2015-01-05 19:15:41 +0100

    Fix for rubinius

--- lib/rom/enumerable_dataset.rb
+++ lib/rom/enumerable_dataset.rb
@@ -12,5 +12,10 @@ module ROM
     def sort_by(&block)
       self.class.new(super(&block), header)
     end
+
+    # FIXME: why is this needed for rubinius?
+    def sort!(*args, &block)
+      data.sort!(*args, &block)
+    end
   end
 end
