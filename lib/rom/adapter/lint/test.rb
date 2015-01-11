ef90adefbc2f87b54368b678cb00284fd23e18b5
Peter Suschlik <peter@suschlik.de>
2015-01-11 01:26:31 +0100

    WIP: Extract lint test into common format

--- lib/rom/adapter/lint/test.rb
+++ lib/rom/adapter/lint/test.rb
@@ -1,3 +1,5 @@
+require 'rom/adapter/lint/common'
+
 module ROM
   class Adapter
     module Lint
@@ -19,27 +21,8 @@ module ROM
       module TestAdapter
         attr_reader :adapter, :uri
 
-        def test_schemes
-          assert_respond_to adapter, :schemes,
-            "#{adapter}.schemes must be implemented"
-
-          assert_instance_of Array, adapter.schemes,
-            "#{adapter}.schemes must return an array with supported URI schemes"
-
-          assert adapter.schemes.any?,
-            "#{adapter}.schemes must return at least one supported URI scheme"
-        end
-
-        def test_setup
-          assert_instance_of adapter, adapter_instance
-        end
-
-        def test_dataset_reader
-          assert_respond_to adapter_instance, :[]
-        end
-
-        def test_dataset_predicate
-          assert_respond_to adapter_instance, :dataset?
+        CommonAdapter.each do |name, block|
+          define_method("test_#{name}", &block)
         end
 
         private
@@ -66,16 +49,8 @@ module ROM
       module TestEnumerableDataset
         attr_reader :dataset, :data
 
-        def test_each
-          result = []
-          dataset.each { |tuple| result << tuple }
-          assert_equal result, data,
-            "#{dataset.class}#each must yield tuples"
-        end
-
-        def test_to_a
-          assert_equal dataset.to_a, data,
-            "#{dataset.class}#to_a must cast dataset to an array"
+        CommonEnumerableDataset.each do |name, block|
+          define_method("test_#{name}", &block)
         end
       end
     end
