16a77c9894d10dca3f7714523accd5869c59d73d
Don Morrison <don@elskwid.net>
2015-01-18 18:09:05 -0800

    Add auto-registration setting on ROM::Repsitory
    The settings:
      ROM::Repository.enable_auto_registration!
      ROM::Repository.disable_auto_registration!
    
    Provide a way to turn off the auto-registration of repository adapters
    when they inherit from ROM::Repository. There are cases, especially in
    testing, where this causes problems and unexpected behavior. It is a
    giant global constant afterall.
    
    Using this will mean our specs may be more explicit but it also means we
    can control the environment and test different behavior in isolation
    without workarounds and complications.

--- lib/rom/repository.rb
+++ lib/rom/repository.rb
@@ -5,6 +5,7 @@ module ROM
   #
   # @api public
   class Repository
+
     # Return connection URI associated with the repository
     #
     # @return [String]
@@ -54,6 +55,30 @@ module ROM
     # @api public
     def self.unregister(klass)
       Repository.registered.delete(klass)
+    end
+
+    # Repository auto-registration is on by default
+    @@auto_register_adapters = true
+
+    # Turn on adapter auto-registration when inheriting from Repository
+    #
+    # @example
+    #   Repsitory.enable_auto_registration!
+    #
+    # @api public
+    def self.enable_auto_registration!
+      @@auto_register_adapters = true
+    end
+
+    # Turn off adapter auto-registration when inheriting from Repository
+    #
+    # @example
+    #
+    #   Repository.disable_auto_registration!
+    #
+    # @api public
+    def self.disable_auto_registration!
+      @@auto_register_adapters = false
     end
 
     # @api private
