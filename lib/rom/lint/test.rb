801229df3b856275684bfb9c68d5b98d658b521a
Peter Suschlik <peter@suschlik.de>
2015-02-16 21:01:01 +0100

    Document Lint::Test{,Repository,EnumerableDataset}

--- lib/rom/lint/test.rb
+++ lib/rom/lint/test.rb
@@ -3,7 +3,13 @@ require 'rom/lint/enumerable_dataset'
 
 module ROM
   module Lint
+    # A module that helps to define test methods
     module Test
+      # Defines a test method converting lint failures to assertions
+      #
+      # @param [String] name
+      #
+      # @api private
       def define_test_method(name, &block)
         define_method "test_#{name}" do
           begin
@@ -33,7 +39,20 @@ module ROM
     module TestRepository
       extend ROM::Lint::Test
 
-      attr_reader :identifier, :repository, :uri
+      # Returns the repository identifier e.g. +:memory+
+      #
+      # @api public
+      attr_reader :identifier
+
+      # Returns the repository class
+      #
+      # @api public
+      attr_reader :repository
+
+      # Returns repostory's URI e.g. "super_db://something"
+      #
+      # @api public
+      attr_reader :uri
 
       ROM::Lint::Repository.each_lint do |name, linter|
         define_test_method name do
@@ -59,7 +78,15 @@ module ROM
     module TestEnumerableDataset
       extend ROM::Lint::Test
 
-      attr_reader :dataset, :data
+      # Returns the dataset instance
+      #
+      # @api public
+      attr_reader :dataset
+
+      # Returns the expected data
+      #
+      # @api public
+      attr_reader :data
 
       ROM::Lint::EnumerableDataset.each_lint do |name, linter|
         define_test_method name do
