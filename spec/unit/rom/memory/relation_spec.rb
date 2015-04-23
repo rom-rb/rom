ca830a66169c2b1598648f090854b9b7a5454027
Piotr Solnica <piotr.solnica@gmail.com>
2015-04-23 22:12:26 +0200

    Extract memory dataset specs into memory relation specs

---
+++ spec/unit/rom/memory/relation_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+require 'rom/lint/spec'
+
+require 'rom/memory/dataset'
+require 'rom/memory/relation'
+
+describe ROM::Memory::Relation do
+  subject(:relation) { ROM::Memory::Relation.new(dataset) }
+
+  let(:dataset) do
+    ROM::Memory::Dataset.new([
+      { name: 'Jane', email: 'jane@doe.org', age: 10 },
+      { name: 'Jade', email: 'jade@doe.org', age: 11 },
+      { name: 'Joe', email: 'joe@doe.org', age: 12 }
+    ])
+  end
+
+  describe '#project' do
+    it 'projects tuples with the provided keys' do
+      expect(relation.project(:name, :age)).to match_array([
+        { name: 'Jane', age: 10 },
+        { name: 'Jade', age: 11 },
+        { name: 'Joe', age: 12 }
+      ])
+    end
+  end
+
+  describe '#restrict' do
+    it 'restricts data using criteria hash' do
+      expect(relation.restrict(age: 10)).to match_array([
+        { name: 'Jane', email: 'jane@doe.org', age: 10 }
+      ])
+
+      expect(relation.restrict(age: 10.0)).to match_array([])
+    end
+
+    it 'restricts data using block' do
+      expect(relation.restrict { |tuple| tuple[:age] > 10 }).to match_array([
+        { name: 'Jade', email: 'jade@doe.org', age: 11 },
+        { name: 'Joe', email: 'joe@doe.org', age: 12 }
+      ])
+    end
+  end
+
+  describe '#order' do
+    it 'sorts data using provided attribute names' do
+      expect(relation.order(:name).to_a).to eq([
+        { name: 'Jade', email: 'jade@doe.org', age: 11 },
+        { name: 'Jane', email: 'jane@doe.org', age: 10 },
+        { name: 'Joe', email: 'joe@doe.org', age: 12 }
+      ])
+    end
+  end
+end
