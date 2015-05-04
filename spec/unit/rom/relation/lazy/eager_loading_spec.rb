1d175ac0e9b8d30e9be9ac84168e6fbd01c5b77a
Piotr Solnica <piotr.solnica@gmail.com>
2015-05-04 18:22:31 +0200

    Support nested graph in eager-loading (refs #212)

--- spec/unit/rom/relation/lazy/eager_loading_spec.rb
+++ spec/unit/rom/relation/lazy/eager_loading_spec.rb
@@ -16,28 +16,53 @@ describe 'Eager loading' do
         restrict { |task| names.include?(task[:name]) }
       end
     end
+
+    setup.relation(:tags) do
+      def for_tasks(tasks)
+        titles = tasks.map { |task| task[:title] }
+        restrict { |tag| titles.include?(tag[:task]) }
+      end
+    end
+
+    setup.repositories[:default].dataset(:tags).insert(task: 'be cool', name: 'red')
+    setup.repositories[:default].dataset(:tags).insert(task: 'be cool', name: 'green')
   end
 
   let(:users) { rom.relation(:users) }
   let(:tasks) { rom.relation(:tasks) }
+  let(:tags) { rom.relation(:tags) }
 
   it 'works' do
     expected = [
       {
         name: 'Jane', email: 'jane@doe.org', tasks: [
-          { name: 'Jane', title: 'be cool', priority: 2 }
+          { name: 'Jane', title: 'be cool', priority: 2, tags: [
+            { task: 'be cool', name: 'red' },
+            { task: 'be cool', name: 'green' }
+          ] }
         ]
       }
     ]
 
-    mapper = proc { |users, tasks|
+    map_users = proc { |users, tasks|
       users.map { |user|
         user.merge(tasks: tasks.select { |task| task[:name] == user[:name] })
       }
     }
 
-    user_with_tasks = users.by_name('Jane').eager_load(tasks.for_users) >> mapper
+    map_tasks = proc { |tasks, tags|
+      tasks.map { |task|
+        task.merge(tags: tags.select { |tag| tag[:task] == task[:title] })
+      }
+    }
 
-    expect(user_with_tasks).to match_array(expected)
+    mapper = proc { |users, (tasks, tags)|
+      map_users[users, map_tasks[tasks, tags]]
+    }
+
+    user_with_tasks = users.by_name('Jane')
+      .eager_load(tasks.for_users.eager_load(tags.for_tasks))
+
+    expect(user_with_tasks >> mapper).to match_array(expected)
   end
 end
