514454676e5a7792e5ad87331b835b7eea7c77f7
Piotr Solnica <piotr.solnica@gmail.com>
2015-05-09 01:03:46 +0200

    Add support for multiple combined blocks in mapping DSL

--- spec/integration/mappers/combine_spec.rb
+++ spec/integration/mappers/combine_spec.rb
@@ -16,7 +16,11 @@ describe 'Mapper definition DSL' do
         end
       end
 
-      setup.relation(:users)
+      setup.relation(:users) do
+        def addresses(_users_)
+          [{ city: 'NYC', user: 'Jane' }, { city: 'Boston', user: 'Joe' }]
+        end
+      end
 
       setup.mappers do
         define(:users) do
@@ -43,6 +47,12 @@ describe 'Mapper definition DSL' do
               attribute :name
             end
           end
+
+          combine :addresses, name: :user do
+            model name: 'Test::Address'
+
+            attribute :city
+          end
         end
       end
     end
@@ -51,20 +61,34 @@ describe 'Mapper definition DSL' do
     let(:tasks) { rom.relation(:tasks) }
 
     let(:joe) {
-      Test::User.new(name: 'Joe', email: 'joe@doe.org', tasks: [
-        Test::Task.new(title: 'be nice', meta: { user: 'Joe', priority: 1 }, tags: []),
-        Test::Task.new(title: 'sleep well', meta: { user: 'Joe', priority: 2 }, tags: [])
-      ])
+      Test::User.new(
+        name: 'Joe',
+        email: 'joe@doe.org',
+        tasks: [
+          Test::Task.new(title: 'be nice', meta: { user: 'Joe', priority: 1 }, tags: []),
+          Test::Task.new(title: 'sleep well', meta: { user: 'Joe', priority: 2 }, tags: [])
+        ],
+        addresses: [
+          Test::Address.new(city: 'Boston')
+        ]
+      )
     }
 
     let(:jane) {
-      Test::User.new(name: 'Jane', email: 'jane@doe.org', tasks: [
-        Test::Task.new(
-          title: 'be cool',
-          meta: { user: 'Jane', priority: 2 },
-          tags: [Test::Tag.new(name: 'blue')]
-        )
-      ])
+      Test::User.new(
+        name: 'Jane',
+        email: 'jane@doe.org',
+        tasks: [
+          Test::Task.new(
+            title: 'be cool',
+            meta: { user: 'Jane', priority: 2 },
+            tags: [Test::Tag.new(name: 'blue')]
+          )
+        ],
+        addresses: [
+          Test::Address.new(city: 'NYC')
+        ]
+      )
     }
 
     it 'works' do
@@ -72,8 +96,12 @@ describe 'Mapper definition DSL' do
 
       Test::User.send(:include, Equalizer.new(:name, :email, :tasks))
       Test::Task.send(:include, Equalizer.new(:title, :meta))
+      Test::Address.send(:include, Equalizer.new(:city))
 
-      result = users.as(:entity).combine(tasks.for_users.combine(tasks.tags))
+      result = users.as(:entity).combine(
+        tasks.for_users.combine(tasks.tags),
+        users.addresses
+      )
 
       expect(result).to match_array([joe, jane])
     end
