b26264d0789be53a6e0328109480a00c131749a0
Chris Flipse <cflipse@gmail.com>
2015-05-26 11:30:32 -0400

    add class-level deprecate

--- lib/rom/support/deprecations.rb
+++ lib/rom/support/deprecations.rb
@@ -14,5 +14,20 @@ module ROM
         end
       end
     end
+
+    def deprecate_class_method(old_name, new_name, msg = nil)
+      class_eval do
+        define_singleton_method(old_name) do |*args, &block|
+          warn <<-MSG.gsub(/^\s+/, '')
+            #{self}.#{old_name} is deprecated and will be removed in 1.0.0.
+            Please use #{self}.#{new_name} instead.
+            #{msg}
+            #{caller.detect { |l| !l.include?('lib/rom')}}
+          MSG
+          __send__(new_name, *args, &block)
+        end
+      end
+    end
+
   end
 end
