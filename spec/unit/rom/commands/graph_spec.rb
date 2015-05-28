96600c23420e644e99e2cd56ab6aa8c2b8c2df8c
Piotr Solnica <piotr.solnica@gmail.com>
2015-05-28 13:02:22 +0200

    Restructure specs for Commands::Graph#call

--- spec/unit/rom/commands/graph_spec.rb
+++ spec/unit/rom/commands/graph_spec.rb
@@ -1,8 +1,22 @@
 require 'spec_helper'
 
 describe ROM::Commands::Graph do
+  subject(:command) do
+    create_user.with(user)
+      .combine(create_task.with(task)
+        .combine(create_tags.with(tags)))
+  end
+
   let(:rom) { setup.finalize }
   let(:setup) { ROM.setup(:memory) }
+
+  let(:create_user) { rom.command(:users).create }
+  let(:create_task) { rom.command(:tasks).create }
+  let(:create_tags) { rom.command(:tags).create }
+
+  let(:user) { { name: 'Jane' } }
+  let(:task) { { title: 'One' } }
+  let(:tags) { [{ name: 'red' }, { name: 'green' }, { name: 'blue' }] }
 
   before do
     setup.relation(:users)
@@ -34,40 +48,38 @@ describe ROM::Commands::Graph do
     end
   end
 
-  it 'works' do
-    create_user = rom.command(:users).create
-    create_task = rom.command(:tasks).create
-    create_tags = rom.command(:tags).create
-
-    user = { name: 'Jane' }
-    task = { title: 'One' }
-    tags = [{ name: 'red' }, { name: 'green' }, { name: 'blue' }]
-
-    command = create_user.with(user)
-      .combine(create_task.with(task).combine(create_tags.with(tags)))
-
-    result = command.call
-
-    expect(result).to match_array([
-      { name: 'Jane' },
-      [
-        { user: 'Jane', title: 'One' },
+  describe '#call' do
+    it 'returns nested results' do
+      expect(command.call).to match_array([
+        { name: 'Jane' },
         [
-          { task: 'One', name: 'red' },
-          { task: 'One', name: 'green' },
-          { task: 'One', name: 'blue' }
+          { user: 'Jane', title: 'One' },
+          [
+            { task: 'One', name: 'red' },
+            { task: 'One', name: 'green' },
+            { task: 'One', name: 'blue' }
+          ]
         ]
-      ]
-    ])
+      ])
+    end
 
-    expect(rom.relation(:users)).to match_array([{ name: 'Jane' }])
+    it 'inserts root' do
+      command.call
+      expect(rom.relation(:users)).to match_array([{ name: 'Jane' }])
+    end
 
-    expect(rom.relation(:tasks)).to match_array([{ user: 'Jane', title: 'One' }])
+    it 'inserts root nodes' do
+      command.call
+      expect(rom.relation(:tasks)).to match_array([{ user: 'Jane', title: 'One' }])
+    end
 
-    expect(rom.relation(:tags)).to match_array([
-      { task: 'One', name: 'red' },
-      { task: 'One', name: 'green' },
-      { task: 'One', name: 'blue' }
-    ])
+    it 'inserts nested graph nodes' do
+      command.call
+      expect(rom.relation(:tags)).to match_array([
+        { task: 'One', name: 'red' },
+        { task: 'One', name: 'green' },
+        { task: 'One', name: 'blue' }
+      ])
+    end
   end
 end
