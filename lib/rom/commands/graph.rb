39afa7a3ccfb3006ea09c842ca0413d625ec28b8
Piotr Solnica <piotr.solnica@gmail.com>
2015-05-29 10:03:06 +0200

    Yard for Commands::Graph

--- lib/rom/commands/graph.rb
+++ lib/rom/commands/graph.rb
@@ -9,7 +9,11 @@ module ROM
       include Pipeline
       include Pipeline::Proxy
 
-      attr_reader :root, :nodes
+      # @attr_reader [Command] root The root command
+      attr_reader :root
+
+      # @attr_reader [Array<Command>] nodes The child commands
+      attr_reader :nodes
 
       alias_method :left, :root
       alias_method :right, :nodes
@@ -20,6 +24,22 @@ module ROM
         @nodes = nodes
       end
 
+      # Calls root and all nodes with the result from root
+      #
+      # Graph results are mappable through `combine` operation in mapper DSL
+      #
+      # @example
+      #   create_user = rom.command(:users).create
+      #   create_task = rom.command(:tasks).create
+      #
+      #   command = create_user
+      #     .with(name: 'Jane')
+      #     .combine(create_task.with(title: 'Task'))
+      #
+      #   command.call
+      #
+      # @return [Array] nested array with command results
+      #
       # @api public
       def call(*args)
         left = root.call(*args)
