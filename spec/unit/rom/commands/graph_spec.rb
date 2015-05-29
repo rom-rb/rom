e208d7a4167382f886229363d0b3dd487e5effc2
Piotr Solnica <piotr.solnica@gmail.com>
2015-05-29 09:31:59 +0200

    Extend commands graph spec

--- spec/unit/rom/commands/graph_spec.rb
+++ spec/unit/rom/commands/graph_spec.rb
@@ -1,54 +1,7 @@
 require 'spec_helper'
 
 describe ROM::Commands::Graph do
-  let(:rom) { setup.finalize }
-  let(:setup) { ROM.setup(:memory) }
-
-  let(:create_user) { rom.command(:users).create }
-  let(:create_task) { rom.command(:tasks).create }
-  let(:create_tags) { rom.command(:tags).create }
-
-  let(:user) { { name: 'Jane' } }
-  let(:task) { { title: 'One' } }
-  let(:tags) { [{ name: 'red' }, { name: 'green' }, { name: 'blue' }] }
-
-  before do
-    setup.relation(:users)
-    setup.relation(:tasks)
-    setup.relation(:tags)
-
-    setup.commands(:users) do
-      define(:create) do
-        result :one
-      end
-    end
-
-    setup.commands(:tasks) do
-      define(:create) do
-        result :one
-
-        def execute(task, user)
-          super(task.merge(user: user[:name]))
-        end
-      end
-    end
-
-    setup.commands(:tags) do
-      define(:create) do
-        def execute(tags, task)
-          super(tags.map { |t| t.merge(task: task[:title]) })
-        end
-      end
-    end
-  end
-
-  describe '#call' do
-    subject(:command) do
-      create_user.with(user)
-        .combine(create_task.with(task)
-        .combine(create_tags.with(tags)))
-    end
-
+  shared_examples_for 'a persisted graph' do
     it 'returns nested results' do
       expect(command.call).to match_array([
         # parent users
@@ -74,23 +27,103 @@ describe ROM::Commands::Graph do
       ])
     end
 
-    it 'inserts root' do
-      command.call
-      expect(rom.relation(:users)).to match_array([{ name: 'Jane' }])
+    context 'persisted relations' do
+      before { command.call }
+
+      it 'inserts root' do
+        expect(rom.relation(:users)).to match_array([{ name: 'Jane' }])
+      end
+
+      it 'inserts root nodes' do
+        expect(rom.relation(:tasks)).to match_array([{ user: 'Jane', title: 'One' }])
+      end
+
+      it 'inserts nested graph nodes' do
+        expect(rom.relation(:tags)).to match_array([
+          { name: 'red', task: 'One' },
+          { name: 'green', task: 'One' },
+          { name: 'blue', task: 'One' }
+        ])
+      end
+    end
+  end
+
+  let(:rom) { setup.finalize }
+  let(:setup) { ROM.setup(:memory) }
+
+  let(:create_user) { rom.command(:users).create }
+  let(:create_task) { rom.command(:tasks).create }
+  let(:create_tags) { rom.command(:tags).create }
+
+  let(:create_many_users) { rom.command(:users).create_many }
+  let(:create_many_tasks) { rom.command(:tasks).create_many }
+  let(:create_tags) { rom.command(:tags).create }
+
+  let(:user) { { name: 'Jane' } }
+  let(:task) { { title: 'One' } }
+  let(:tags) { [{ name: 'red' }, { name: 'green' }, { name: 'blue' }] }
+
+  before do
+    setup.relation(:users)
+    setup.relation(:tasks)
+    setup.relation(:tags)
+
+    setup.commands(:users) do
+      define(:create) do
+        result :one
+      end
+
+      define(:create) do
+        register_as :create_many
+      end
     end
 
-    it 'inserts root nodes' do
-      command.call
-      expect(rom.relation(:tasks)).to match_array([{ user: 'Jane', title: 'One' }])
+    setup.commands(:tasks) do
+      define(:create) do
+        result :one
+
+        def execute(task, user)
+          super(task.merge(user: user[:name]))
+        end
+      end
+
+      define(:create) do
+        register_as :create_many
+
+        def execute(tasks, user)
+          super(tasks.map { |t| t.merge(user: user[:name]) })
+        end
+      end
     end
 
-    it 'inserts nested graph nodes' do
-      command.call
-      expect(rom.relation(:tags)).to match_array([
-        { name: 'red', task: 'One' },
-        { name: 'green', task: 'One' },
-        { name: 'blue', task: 'One' }
-      ])
+    setup.commands(:tags) do
+      define(:create) do
+        def execute(tags, task)
+          super(tags.map { |t| t.merge(task: task[:title]) })
+        end
+      end
+    end
+  end
+
+  describe '#call' do
+    context 'when result is :one in root and its direct children' do
+      it_behaves_like 'a persisted graph' do
+        subject(:command) do
+          create_user.with(user)
+            .combine(create_task.with(task)
+            .combine(create_tags.with(tags)))
+        end
+      end
+    end
+
+    context 'when result is :many in root and its direct children' do
+      it_behaves_like 'a persisted graph' do
+        subject(:command) do
+          create_many_users.with([user])
+            .combine(create_many_tasks.with([task])
+            .combine(create_tags.with(tags)))
+        end
+      end
     end
   end
 
