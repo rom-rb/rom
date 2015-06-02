ddf80c238fe6adeb922cb7d70b83e9feb139928c
Andrew Kozin <andrew.kozin@gmail.com>
2015-06-02 10:50:12 +0300

    Make `prefix` and `prefix_separator` applicable inside the block
    Both operations can be used either from root or inside the block.
    They change prefix / separator for the attributes following them.
    
      setup.mappers do
        define(:users) do
          prefix :user
    
          attribute :name
          wrap :contact do
            attribute :skype
    
            prefix :contact
            prefix_separator '.'
            attribute 'email'
          end
        end
      end
    
      users.first
      # => { user_name: 'Joe', user_skype: 'joe', 'contact.email' => 'joe@doe.org' }
      users.as(:users).first
      # => { user: 'Joe', contact: { skype: 'joe', 'email': 'joe@doe.org' } }
    
    If the target key not symbolized, the prefixed source is treated as a string too

---
+++ spec/integration/mappers/prefix_separator_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+require 'rom/memory'
+
+describe 'Mapper definition DSL' do
+  let(:setup) { ROM.setup(:memory) }
+  let(:rom)   { ROM.finalize.env   }
+
+  before do
+    setup.relation(:users)
+
+    users = setup.default.dataset(:users)
+    users.insert(
+      user_id: 1,
+      user_name: 'Joe',
+      user_email: 'joe@doe.com',
+      'user.skype' => 'joe',
+      :'user.phone' => '1234567890'
+    )
+  end
+
+  describe 'prefix' do
+    subject(:mapped_users) { rom.relation(:users).as(:users).to_a }
+
+    it 'applies new separator to the attributes following it' do
+      setup.mappers do
+        define(:users) do
+
+          prefix :user
+          attribute :id
+          attribute :name
+          wrap :contacts do
+            attribute :email
+
+            prefix_separator '.'
+            attribute 'skype'
+            attribute :phone
+          end
+        end
+      end
+
+      expect(mapped_users).to eql [
+        {
+          id: 1,
+          name: 'Joe',
+          contacts: {
+            email: 'joe@doe.com',
+            'skype' => 'joe',
+            phone: '1234567890'
+          }
+        }
+      ]
+    end
+  end
+end
