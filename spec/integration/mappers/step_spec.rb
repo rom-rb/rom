21d84e0679dbc7c4f82893ae2116de4a395df1a4
Andrew Kozin <andrew.kozin@gmail.com>
2015-06-04 15:37:56 +0300

    Added specs for `step` mapper operation

--- spec/integration/mappers/step_spec.rb
+++ spec/integration/mappers/step_spec.rb
@@ -1,65 +1,120 @@
 require 'spec_helper'
 
 describe 'Mapper definition DSL' do
-  include_context 'users and tasks'
+  let(:setup) { ROM.setup(:memory) }
+  let(:rom)   { ROM.finalize.env   }
 
-  let(:header) { mapper.header }
+  before do
+    setup.relation(:lists)
 
-  describe 'stepped mapper' do
-    before do
-      setup.relation(:tasks) do
-        def with_users
-          join(users)
-        end
-      end
+    setup.default.dataset(:lists).insert(
+      list_id: 1,
+      list_tasks: [
+        { user: 'Jacob', task_id: 1, task_title: 'be nice'    },
+        { user: 'Jacob', task_id: 2, task_title: 'sleep well' }
+      ]
+    )
+  end
 
-      setup.relation(:users) do
-        def with_tasks
-          join(tasks)
-        end
-      end
+  describe 'step' do
+    let(:mapped) { rom.relation(:lists).as(:lists).to_a }
 
+    it 'applies transformations one by one' do
       setup.mappers do
-        define(:users) do
+        define(:lists) do
+          step do
+            prefix 'list'
+            attribute :id
+            unfold :tasks
+          end
+
+          step do
+            unwrap :tasks do
+              attribute :task_id
+              attribute :name, from: :user
+              attribute :task_title
+            end
+          end
+
+          step do
+            group :tasks do
+              prefix 'task'
+              attribute :id
+              attribute :title
+            end
+          end
+
+          step do
+            wrap :user do
+              attribute :name
+              attribute :tasks
+            end
+          end
         end
       end
+
+      expect(mapped).to eql [
+        {
+          id: 1,
+          user: {
+            name: 'Jacob',
+            tasks: [
+              { id: 1, title: 'be nice'    },
+              { id: 2, title: 'sleep well' }
+            ]
+          }
+        }
+      ]
     end
 
-    it 'processes the mapper step by step' do
+    it 'applies settings from root' do
       setup.mappers do
-        define(:with_tasks, parent: :users) do
+        define(:lists) do
+
+          prefix 'list'
 
           step do
-            attribute :an_email, from: :email
+            attribute :id
+            attribute :tasks
           end
-
-          step do
-            attribute :name
-            attribute :title
-            attribute :priority
-            attribute :email, from: :an_email
-          end
-
-          step do
-            model name: 'Test::UserWithTasks'
-            attribute :name
-            attribute :email
-            group tasks: [:title, :priority]
-          end
-
         end
       end
 
-      rom = setup.finalize
-
-      Test::UserWithTasks.send(:include, Equalizer.new(:name, :email, :tasks))
-
-      jane = rom.relation(:users).with_tasks.map_with(:with_tasks).to_a.last
-
-      expect(jane.name).to eql('Jane')
-      expect(jane.email).to eql('jane@doe.org')
-      expect(jane.tasks).to eql([{ title: 'be cool', priority: 2 }])
+      expect(mapped).to eql [
+        {
+          id: 1,
+          tasks: [
+            { user: 'Jacob', task_id: 1, task_title: 'be nice'    },
+            { user: 'Jacob', task_id: 2, task_title: 'sleep well' }
+          ]
+        }
+      ]
     end
 
+    it 'cannot precede attributes' do
+      setup.mappers do
+        define(:lists) do
+          step do
+            unfold :tasks, from: :list_tasks
+          end
+          attribute :id, from: :list_id
+        end
+      end
+
+      expect { rom }.to raise_error ROM::MapperMisconfiguredError
+    end
+
+    it 'cannot succeed attributes' do
+      setup.mappers do
+        define(:lists) do
+          attribute :id, from: :list_id
+          step do
+            unfold :tasks, from: :list_tasks
+          end
+        end
+      end
+
+      expect { rom }.to raise_error ROM::MapperMisconfiguredError
+    end
   end
 end
