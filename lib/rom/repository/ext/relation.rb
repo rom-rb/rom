6a8e420aad16882eea6354770c696ec8db6160b3
Piotr Solnica <piotr.solnica@gmail.com>
2015-07-30 11:37:23 +0200

    Remove extensions that are no longer needed

--- lib/rom/repository/ext/relation.rb
+++ lib/rom/repository/ext/relation.rb
@@ -1,12 +1,9 @@
 module ROM
   module SQL
     class Relation < ROM::Relation
-      # TODO: This will go away when ROM core generates Lazy classes dedicated for each
-      # relation class and by adding configuration for the query interface which
-      # could be easily exposed by lazy relations for cases like repository
       def self.inherited(klass)
         super
-        klass.exposed_relations.merge(Set[:columns, :primary_key, :foreign_key, :for_combine])
+        klass.exposed_relations.merge(Set[:columns, :for_combine])
       end
 
       def for_combine(keys, relation)
@@ -16,18 +13,12 @@ module ROM
     end
   end
 
-  # TODO: consider moving this to rom core
   class Relation
     class Lazy
-      # TODO: this will go away when Lazy exposes the whole query interface
-      undef_method :select # Object#select bites me once again
-
       def base_name
         relation.name
       end
-    end
 
-    class Curried < Lazy
       def columns
         relation.columns
       end
