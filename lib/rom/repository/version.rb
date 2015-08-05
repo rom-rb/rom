bfbfe268d8338a0f2b9c9ea0cf9dee29f0a5ce8f
Piotr Solnica <piotr.solnica@gmail.com>
2015-08-05 15:01:06 +0200

    Remove require of rom/gateway from version file (why do we even use those files?)

--- lib/rom/repository/version.rb
+++ lib/rom/repository/version.rb
@@ -1,6 +1,8 @@
-require 'rom/gateway'
-
 module ROM
+  # TODO: remove this once deprecated Repository is gone in rom core
+  class Gateway
+  end
+
   class Repository < Gateway
     VERSION = '0.0.1'.freeze
   end
