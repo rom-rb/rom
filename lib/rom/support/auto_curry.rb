51aea8bbd9dc15393cd8f3427ab84d4ce97049ad
Piotr Solnica <piotr.solnica@gmail.com>
2015-08-07 14:13:31 +0200

    Add auto_curry interface

--- lib/rom/support/auto_curry.rb
+++ lib/rom/support/auto_curry.rb
@@ -2,26 +2,27 @@ module ROM
   module AutoCurry
     def self.extended(klass)
       busy = false
-      curried = klass.curried
 
       klass.define_singleton_method(:method_added) do |name|
         return if busy
-
         busy = true
-        meth = instance_method(name)
-        arity = meth.arity
-
-        define_method(name) do |*args|
-          if arity < 0 || arity == args.size
-            meth.bind(self).(*args)
-          else
-            curried.new(self, name: name, curry_args: args, arity: arity)
-          end
-        end
-
+        auto_curry(name)
         busy = false
-
         super(name)
+      end
+    end
+
+    def auto_curry(name)
+      curried = self.curried
+      meth = instance_method(name)
+      arity = meth.arity
+
+      define_method(name) do |*args|
+        if arity < 0 || arity == args.size
+          meth.bind(self).(*args)
+        else
+          curried.new(self, name: name, curry_args: args, arity: arity)
+        end
       end
     end
   end
