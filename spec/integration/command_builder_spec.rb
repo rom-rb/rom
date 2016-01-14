a5bd0e3baa3472c271d5b2c62d2b3a1cf3520e97
Piotr Solnica <piotr.solnica@gmail.com>
2016-01-14 14:01:24 +0100

    Extend spec with a deeply nested graph example

--- spec/integration/command_builder_spec.rb
+++ spec/integration/command_builder_spec.rb
@@ -28,6 +28,23 @@ RSpec.describe 'Building commands' do
       expect(user.task.title).to eql('Task one')
     end
 
+    it 'builds Create command for a deeply nested relation graph' do
+      create_user = repo.command(
+        :create,
+        repo.users.combine_children(one: repo.tasks.combine_children(many: repo.tags))
+      )
+
+      user = create_user.call(
+        user: { name: 'Jane Doe', task: { title: 'Task one', tags: [{ name: 'red' }] } }
+      ).one
+
+      expect(user.id).to_not be(nil)
+      expect(user.name).to eql('Jane Doe')
+      expect(user.task.title).to eql('Task one')
+      expect(user.task.tags).to be_instance_of(Array)
+      expect(user.task.tags.first.name).to eql('red')
+    end
+
     it 'builds Create command for a relation graph with one-to-many' do
       create_user = repo.command(
         :create,
