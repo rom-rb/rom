8d2c9f56447c5025f3cbfb801d515a9e7a163fda
Nikita Shilnikov <ns@latera.ru>
2016-07-09 15:19:09 +0300

    Improve relations finalizer

--- lib/rom/setup/finalize/finalize_relations.rb
+++ lib/rom/setup/finalize/finalize_relations.rb
@@ -22,33 +22,40 @@ module ROM
       def run!
         RelationRegistry.new do |registry, relations|
           @relation_classes.each do |klass|
-            # TODO: raise a meaningful error here and add spec covering the case
-            #       where klass' gateway points to non-existant repo
-            gateway = @gateways.fetch(klass.gateway)
-            ds_proc = klass.dataset_proc || -> _ { self }
+            relation = build_relation(klass, registry)
 
-            if klass.schema && !klass.schema.defined?
-              klass.schema.infer!(gateway)
-            end
-            dataset = gateway.dataset(klass.dataset).instance_exec(klass, &ds_proc)
+            key = relation.name.to_sym
 
-            relation = klass.new(dataset, __registry__: registry)
-
-            name = relation.name.to_sym
-
-            if registry.key?(name)
+            if registry.key?(key)
               raise RelationAlreadyDefinedError,
-                    "Relation with `register_as #{name.inspect}` registered more " \
+                    "Relation with `register_as #{key.inspect}` registered more " \
                     "than once"
             end
 
-            relations[name] = relation
+            relations[key] = relation
           end
 
           relations.each_value do |relation|
             relation.class.finalize(registry, relation)
           end
         end
+      end
+
+      # @return [ROM::Relation]
+      #
+      # @api private
+      def build_relation(klass, registry)
+        # TODO: raise a meaningful error here and add spec covering the case
+        #       where klass' gateway points to non-existant repo
+        gateway = @gateways.fetch(klass.gateway)
+        ds_proc = klass.dataset_proc || -> _ { self }
+
+        if klass.schema && !klass.schema.defined?
+          klass.schema.infer!(gateway)
+        end
+        dataset = gateway.dataset(klass.dataset).instance_exec(klass, &ds_proc)
+
+        klass.new(dataset, __registry__: registry)
       end
     end
   end
