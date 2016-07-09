dfd7339bd2e1bc4cf030200ef6e8cecc06d7a4ef
Nikita Shilnikov <ns@latera.ru>
2016-07-09 15:06:37 +0300

    Add docs for Relation::Name

--- lib/rom/relation/name.rb
+++ lib/rom/relation/name.rb
@@ -1,8 +1,23 @@
 module ROM
   class Relation
+    # Relation name container
+    #
+    # This is a simple struct with two fields.
+    # It handles both relation registration name (i.e. Symbol) and dataset name.
+    # The reason we need it is a simplification of passing around these two objects.
+    # It is quite common to have a dataset named differently from a relation
+    # built on top if you are dealing with a legacy DB and often you need both
+    # to support things such as associations (rom-sql as an example).
+    #
+    # @api private
     class Name
       include Dry::Equalizer(:relation, :dataset)
 
+      # Coerce an object to a Name instance
+      #
+      # @return [ROM::Relation::Name]
+      #
+      # @api private
       def self.[](relation, dataset = nil)
         if relation.is_a?(Name)
           relation
@@ -11,15 +26,31 @@ module ROM
         end
       end
 
+      # Relation registration name
+      #
+      # @return [Symbol]
+      #
+      # @api private
       attr_reader :relation
 
+      # Underlying dataset name
+      #
+      # @return [Symbol]
+      #
+      # @api private
       attr_reader :dataset
 
+      # @api private
       def initialize(relation, dataset = nil)
         @relation = relation
         @dataset = dataset || relation
       end
 
+      # Return relation name
+      #
+      # @return [String]
+      #
+      # @api private
       def to_s
         if relation == dataset
           relation
@@ -28,14 +59,29 @@ module ROM
         end
       end
 
+      # Alias for registration key implicitly called by ROM::Registry
+      #
+      # @return [Symbol]
+      #
+      # @api private
       def to_sym
         relation
       end
 
+      # Return inspected relation
+      #
+      # @return [String]
+      #
+      # @api private
       def inspect
-        "ROM::Relation::Name(#{to_s})"
+        "#{self.class.name}(#{self})"
       end
 
+      # Build a new name. Useful for Curried and other relation proxies
+      #
+      # @return [ROM::Relation::Name]
+      #
+      # @api private
       def with(relation)
         self.class.new(relation, dataset)
       end
