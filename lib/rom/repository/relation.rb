9b957077ffedfaa7f013df4fbd438c8014f35b3b
Piotr Solnica <piotr.solnica+oss@gmail.com>
2016-07-20 13:13:35 +0200

    Remove Relation#fetch patch since rom-sql has it now

--- lib/rom/repository/relation.rb
+++ lib/rom/repository/relation.rb
@@ -5,11 +5,6 @@ module ROM
   # TODO: these APIs are candidates for inclusion into rom core
   class Relation
     # @api public
-    def fetch(pk)
-      where(primary_key => pk).one!
-    end
-
-    # @api public
     def changeset(*args)
       ROM.Changeset(self, *args)
     end
