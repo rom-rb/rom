06af22ee6938c0e7791dd54262c41ce90babcb17
Piotr Solnica <piotr.solnica+oss@gmail.com>
2016-11-11 15:13:23 +0100

    Update API docs for ROM::Container
    Closes #352

--- lib/rom/container.rb
+++ lib/rom/container.rb
@@ -4,7 +4,97 @@ require 'rom/commands/graph/builder'
 require 'rom/support/publisher'
 
 module ROM
-  # Exposes defined gateways, relations and mappers
+  # ROM container is an isolated environment with no global state where all
+  # components are registered. Container objects provide access to your
+  # relations, commands and mappers. ROM containers are usually configured and
+  # handled via framework integrations, although it is easy to use them
+  # standalone.
+  #
+  # There are 3 types of container setup:
+  #
+  # * in-line setup - a simple block-based configuration which allows configuring
+  #   all components and gives you back a container instance. This type is suitable
+  #   for small scripts, or in some cases rake tasks
+  # * multi-step setup - this type requires creating a configuration object,
+  #   registering component classes (ie relation classes) and passing the config
+  #   to container builder function. This type is suitable when your environment
+  #   is not typical and you need full control over component registration
+  # * multi-step setup with auto-registration - same as multi-step but allows
+  #   you to configure auto-registration mechanism which will register component
+  #   classes for you, based on dir/file naming conventions. This is the most
+  #   common type of setup that's used by framework integrations
+  #
+  # @example in-line setup
+  #   rom = ROM.container(:sql, 'sqlite::memory') do |config|
+  #     config.default.create_table :users do
+  #       primary_key :id
+  #       column :name, String, null: false
+  #     end
+  #
+  #     config.relation(:users) do
+  #       schema(infer: true)
+  #
+  #       def by_name(name)
+  #         where(name: name)
+  #       end
+  #     end
+  #   end
+  #
+  #   rom.relations[:users].insert(name: "Jane")
+  #
+  #   rom.relations[:users].by_name("Jane").to_a
+  #   # [{:id=>1, :name=>"Jane"}]
+  #
+  # @example multi-step setup with explicit component classes
+  #   config = ROM::Configuration.new(:sql, 'sqlite::memory')
+  #
+  #   config.default.create_table :users do
+  #     primary_key :id
+  #     column :name, String, null: false
+  #   end
+  #
+  #   class Users < ROM::Relation[:sql]
+  #     schema(:users, infer: true)
+  #
+  #     def by_name(name)
+  #       where(name: name)
+  #     end
+  #   end
+  #
+  #   config.register_relation(Users)
+  #
+  #   rom = ROM.container(config)
+  #
+  #   rom.relations[:users].insert(name: "Jane")
+  #
+  #   rom.relations[:users].by_name("Jane").to_a
+  #   # [{:id=>1, :name=>"Jane"}]
+  #
+  #
+  # @example multi-step setup with auto-registration
+  #   config = ROM::Configuration.new(:sql, 'sqlite::memory')
+  #   config.auto_registration('./persistence', namespace: false)
+  #
+  #   config.default.create_table :users do
+  #     primary_key :id
+  #     column :name, String, null: false
+  #   end
+  #
+  #   # ./persistence/relations/users.rb
+  #   class Users < ROM::Relation[:sql]
+  #     schema(infer: true)
+  #
+  #     def by_name(name)
+  #       where(name: name)
+  #     end
+  #   end
+  #
+  #   rom = ROM.container(config)
+  #
+  #   rom.relations[:users].insert(name: "Jane")
+  #
+  #   rom.relations[:users].by_name("Jane").to_a
+  #   # [{:id=>1, :name=>"Jane"}]
   #
   # @api public
   class Container
@@ -18,12 +108,12 @@ module ROM
 
     # @return [RelationRegistry] relation registry
     #
-    # @api private
+    # @api public
     attr_reader :relations
 
     # @return [Registry] command registry
     #
-    # @api private
+    # @api public
     attr_reader :commands
 
     # @return [Registry] mapper registry
@@ -39,7 +129,11 @@ module ROM
       @commands = commands
     end
 
-    # Get lazy relation identified by its name
+    # Get relation instance identified by its name
+    #
+    # This method will use a custom mapper if it was configured. ie if you have
+    # a relation called `:users` and a mapper configured for `:users` relation,
+    # then by default this mapper will be used.
     #
     # @example
     #   rom.relation(:users)
@@ -58,7 +152,7 @@ module ROM
     #
     # @yield [Relation]
     #
-    # @return [Relation::Lazy]
+    # @return [Relation]
     #
     # @api public
     def relation(name, &block)
@@ -79,8 +173,7 @@ module ROM
     # Returns commands registry for the given relation
     #
     # @example
-    #
-    #   # plain command returning tuples
+    #   # plain command without mapping
     #   rom.command(:users).create
     #
     #   # allows auto-mapping using registered mappers
@@ -121,6 +214,16 @@ module ROM
       end
     end
 
+    # Disconnect all gateways
+    #
+    # @example
+    #   rom = ROM.container(:sql, 'sqlite://my_db.sqlite')
+    #   rom.relations[:users].insert(name: "Jane")
+    #   rom.disconnect
+    #
+    # @return [Hash<Symbol=>Gateway>] a hash with disconnected gateways
+    #
+    # @api public
     def disconnect
       gateways.each_value(&:disconnect)
     end
