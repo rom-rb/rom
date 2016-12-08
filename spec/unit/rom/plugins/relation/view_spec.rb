ad7bcc1ba1c2f357e97d13a9a3650714d28e1da5
Piotr Solnica <piotr.solnica+oss@gmail.com>
2016-12-08 14:15:04 +0100

    Wrap view spec examples in a context

--- spec/unit/rom/plugins/relation/view_spec.rb
+++ spec/unit/rom/plugins/relation/view_spec.rb
@@ -5,47 +5,49 @@ require 'rom/plugins/relation/view'
 RSpec.describe ROM::Plugins::Relation::View do
   subject(:relation) { relation_class.new([]) }
 
-  let(:relation_class) do
-    Class.new(ROM::Memory::Relation) do
-      use :view
+  context 'without a schema' do
+    let(:relation_class) do
+      Class.new(ROM::Memory::Relation) do
+        use :view
 
-      view(:base, [:id, :name]) do
-        self
+        view(:base, [:id, :name]) do
+          self
+        end
+
+        view(:ids, [:id]) do
+          self
+        end
+
+        view(:names, [:name]) do |id|
+          self
+        end
+      end
+    end
+
+    describe '#attributes' do
+      it 'returns base view attributes by default' do
+        expect(relation.attributes).to eql([:id, :name])
       end
 
-      view(:ids, [:id]) do
-        self
+      it 'returns attributes for a view' do
+        expect(relation.ids.attributes).to eql([:id])
       end
 
-      view(:names, [:name]) do |id|
-        self
+      it 'returns attributes for a view with args' do
+        expect(relation.names(1).attributes).to eql([:name])
       end
-    end
-  end
 
-  describe '#attributes' do
-    it 'returns base view attributes by default' do
-      expect(relation.attributes).to eql([:id, :name])
-    end
+      it 'returns attributes for a curried view' do
+        expect(relation.names.attributes).to eql([:name])
+      end
 
-    it 'returns attributes for a view' do
-      expect(relation.ids.attributes).to eql([:id])
-    end
+      it 'returns correct arity for a curried view' do
+        expect(relation.names.arity).to be(1)
+      end
 
-    it 'returns attributes for a view with args' do
-      expect(relation.names(1).attributes).to eql([:name])
-    end
-
-    it 'returns attributes for a curried view' do
-      expect(relation.names.attributes).to eql([:name])
-    end
-
-    it 'returns correct arity for a curried view' do
-      expect(relation.names.arity).to be(1)
-    end
-
-    it 'returns explicitly set attributes' do
-      expect(relation.with(attributes: [:foo, :bar]).attributes).to eql([:foo, :bar])
+      it 'returns explicitly set attributes' do
+        expect(relation.with(attributes: [:foo, :bar]).attributes).to eql([:foo, :bar])
+      end
     end
   end
 end
