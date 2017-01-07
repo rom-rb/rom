b6c2e249ff9c4e75f39c01d3d533fab8dc69a4c1
Piotr Solnica <piotr.solnica+oss@gmail.com>
2017-01-07 13:51:36 +0100

    Add unit specs for Repository#changeset

---
+++ spec/unit/repository/changeset_spec.rb
@@ -0,0 +1,75 @@
+RSpec.describe ROM::Repository, '#changeset' do
+  subject(:repo) do
+    Class.new(ROM::Repository) { relations :users }.new(rom)
+  end
+
+  include_context 'database'
+  include_context 'relations'
+
+  describe ROM::Changeset::Create do
+    let(:changeset) do
+      repo.changeset(:users, name: 'Jane')
+    end
+
+    it 'has data' do
+      expect(changeset.to_h).to eql(name: 'Jane')
+    end
+
+    it 'has relation' do
+      expect(changeset.relation).to be(repo.users)
+    end
+
+    it 'can return a dedicated command' do
+      expect(changeset.command.call).to eql(id: 1, name: 'Jane')
+    end
+  end
+
+  describe ROM::Changeset::Update do
+    let(:changeset) do
+      repo.changeset(:users, user[:id], name: 'Jane Doe')
+    end
+
+    let(:user) do
+      repo.command(:create, repo.users).call(name: 'Jane')
+    end
+
+    it 'has data' do
+      expect(changeset.to_h).to eql(name: 'Jane Doe')
+    end
+
+    it 'has diff' do
+      expect(changeset.diff).to eql(name: 'Jane Doe')
+    end
+
+    it 'has relation' do
+      expect(changeset.relation).to be(repo.users)
+    end
+
+    it 'can return a dedicated command' do
+      expect(changeset.command.call).to eql(id: 1, name: 'Jane Doe')
+    end
+  end
+
+  describe ROM::Changeset::Delete do
+    let(:changeset) do
+      repo.changeset(delete: relation)
+    end
+
+    let(:relation) do
+      repo.users.by_pk(user[:id])
+    end
+
+    let(:user) do
+      repo.command(:create, repo.users).call(name: 'Jane')
+    end
+
+    it 'has relation' do
+      expect(changeset.relation).to eql(relation)
+    end
+
+    it 'can return a dedicated command' do
+      expect(changeset.command.call.to_h).to eql(id: 1, name: 'Jane')
+      expect(relation.one).to be(nil)
+    end
+  end
+end
