c5dc4bd67e9bb99753cd4dcddf37498ae4987227
Piotr Solnica <piotr.solnica+oss@gmail.com>
2017-01-22 12:25:59 +0100

    Add YARD docs for Changeset::Delete

--- lib/rom/repository/changeset/delete.rb
+++ lib/rom/repository/changeset/delete.rb
@@ -1,11 +1,25 @@
 module ROM
   class Changeset
+    # Changeset specialization for delete commands
+    #
+    # Delete changesets will execute delete command for its relation, which
+    # means proper restricted relations should be used with this changeset.
+    #
+    # @api public
     class Delete < Changeset
+      # Return command for this changesets
+      #
+      # @return [Command]
+      #
       # @api private
       def command
         command_compiler.(command_type, relation, mapper: false)
       end
 
+      # Return command type identifier for this changeset
+      #
+      # @return [Symbol]
+      #
       # @api private
       def default_command_type
         :delete
