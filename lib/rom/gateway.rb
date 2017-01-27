9f5e235d73b7437ee8d3ea9d111d9118a3ddb026
Piotr Solnica <piotr.solnica+oss@gmail.com>
2017-01-27 11:02:21 +0100

    YARD fixes/updates

--- lib/rom/gateway.rb
+++ lib/rom/gateway.rb
@@ -5,49 +5,61 @@ require 'rom/transaction'
 module ROM
   # Abstract gateway class
   #
+  # Every adapter needs to inherit from this class and implement
+  # required interface
+  #
+  # @abstract
+  #
   # @api public
   class Gateway
     extend Dry::Core::ClassAttributes
 
     defines :adapter
 
-    # Return connection object
-    #
-    # @return [Object] type varies depending on the gateway
-    #
-    # @api public
+    # @!attribute [r] connection
+    #   @return [Object] The gateway's connection object (type varies across adapters)
     attr_reader :connection
 
-    # Setup a gateway
+    # Set up a gateway
     #
     # @overload setup(type, *args)
     #   Sets up a single-gateway given a gateway type.
     #   For custom gateways, create an instance and pass it directly.
     #
-    #   @param [Symbol] type
-    #   @param [Array] *args
+    #   @example
+    #     module SuperDB
+    #       class Gateway < ROM::Gateway
+    #         def initialize(options)
+    #         end
+    #       end
+    #     end
+    #
+    #     ROM.register_adapter(:super_db, SuperDB)
+    #
+    #     Gateway.setup(:super_db, some: 'options')
+    #     # SuperDB::Gateway.new(some: 'options') is called
+    #
+    #   @param [Symbol] type Registered gateway identifier
+    #   @param [Array] args Additional gateway options
     #
     # @overload setup(gateway)
+    #   Set up a gateway instance
+    #
+    #   @example
+    #     module SuperDB
+    #       class Gateway < ROM::Gateway
+    #         def initialize(options)
+    #         end
+    #       end
+    #     end
+    #
+    #     ROM.register_adapter(:super_db, SuperDB)
+    #
+    #     Gateway.setup(SuperDB::Gateway.new(some: 'options'))
+    #
     #   @param [Gateway] gateway
     #
     # @return [Gateway] a specific gateway subclass
-    #
-    # @example
-    #   module SuperDB
-    #     class Gateway < ROM::Gateway
-    #       def initialize(options)
-    #       end
-    #     end
-    #   end
-    #
-    #   ROM.register_adapter(:super_db, SuperDB)
-    #
-    #   Gateway.setup(:super_db, some: 'options')
-    #   # SuperDB::Gateway.new(some: 'options') is called
-    #
-    #   # or alternatively
-    #   super_db = Gateway.setup(SuperDB::Gateway.new(some: 'options'))
-    #   Gateway.setup(super_db)
     #
     # @api public
     def self.setup(gateway_or_scheme, *args)
@@ -76,7 +88,7 @@ module ROM
 
     # Get gateway subclass for a specific adapter
     #
-    # @param [Symbol] type adapter identifier
+    # @param [Symbol] type Adapter identifier
     #
     # @return [Class]
     #
@@ -109,12 +121,21 @@ module ROM
 
     # A generic interface for setting up a logger
     #
+    # This is not a required interface, it's a no-op by default
+    #
+    # @abstract
+    #
     # @api public
     def use_logger(*)
       # noop
     end
 
     # A generic interface for returning default logger
+    #
+    # Adapters should implement this method as handling loggers is different
+    # across adapters. This is a no-op by default and returns nil.
+    #
+    # @return [NilClass]
     #
     # @api public
     def logger
@@ -123,8 +144,10 @@ module ROM
 
     # Extension hook for adding gateway-specific behavior to a command class
     #
-    # @param [Class] klass command class
-    # @param [Object] _dataset dataset that will be used with this command class
+    # This simply returns back the class by default
+    #
+    # @param [Class] klass The command class
+    # @param [Object] _dataset The dataset that will be used with this command class
     #
     # @return [Class]
     #
@@ -137,7 +160,7 @@ module ROM
     #
     # Every gateway that supports schema inference should implement this method
     #
-    # @return [Array] array with datasets and their names
+    # @return [Array] An array with dataset names
     #
     # @api private
     def schema
@@ -162,6 +185,8 @@ module ROM
     def transaction(opts = EMPTY_HASH, &block)
       transaction_runner(opts).run(opts, &block)
     end
+
+    private
 
     # @api private
     def transaction_runner(_)
