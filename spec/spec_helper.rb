fccf719cccff7c8c456c956f0b4600f8b41e1a69
Piotr Solnica <piotr.solnica+oss@gmail.com>
2017-02-24 17:47:41 +0100

    Report coverage on MRI 2.4.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-if ENV['COVERAGE'] == 'true' && RUBY_ENGINE == 'ruby' && RUBY_VERSION == '2.3.1'
+if ENV['COVERAGE'] == 'true' && RUBY_ENGINE == 'ruby' && RUBY_VERSION == '2.4.0'
   require "simplecov"
   SimpleCov.start do
     add_filter '/spec/'
