e05b5ebc95cf23a41d60d08e61db4348b5a38b72
Piotr Solnica <piotr.solnica+oss@gmail.com>
2017-06-24 12:42:07 +0200

    Make StructBuilder inherit from Dry::Types::Compiler
    This is really an extension of the dry-types compiler so it makes sense
    to inherit from it

--- mapper/lib/rom/struct_builder.rb
+++ mapper/lib/rom/struct_builder.rb
@@ -1,7 +1,9 @@
 require 'dry/core/inflector'
 require 'dry/core/class_builder'
+require 'dry/types/compiler'
 
 require 'rom/initializer'
+require 'rom/types'
 require 'rom/cache'
 require 'rom/struct'
 require 'rom/open_struct'
@@ -9,9 +11,10 @@ require 'rom/schema/attribute'
 
 module ROM
   # @api private
-  class StructBuilder
+  class StructBuilder < Dry::Types::Compiler
     extend Initializer
 
+    param :registry, default: -> { Dry::Types }
     option :namespace, reader: true, default: -> { ROM::Struct }
     option :cache, reader: true, default: -> { Cache.new }
 
@@ -40,11 +43,6 @@ module ROM
 
     private
 
-    def visit(ast)
-      name, node = ast
-      __send__("visit_#{name}", node)
-    end
-
     def visit_relation(node)
       relation_name, header, meta = node
       name = meta[:combine_name] || relation_name.relation
@@ -71,20 +69,14 @@ module ROM
       [meta[:alias] && !meta[:wrapped] ? meta[:alias] : name, visit(type).meta(meta)]
     end
 
-    def visit_definition(node)
-      type, meta = node
-
-      Dry::Types::Definition.new(type, meta: meta)
-    end
-
-    def visit_sum(node)
-      left, right, meta = node
-
-      visit(right)
-    end
-
     def visit_constructor(node)
       definition, fn_register_name, meta = node
+
+      visit(definition)
+    end
+
+    def visit_constrained(node)
+      definition, rule = node
 
       visit(definition)
     end
