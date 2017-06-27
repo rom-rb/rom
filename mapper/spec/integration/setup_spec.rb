b3bdca59a5affdab1df2e60c64a3d825cd53f06e
GustavoCaso <gustavocaso@gmail.com>
2017-06-27 07:59:21 +0200

    Added specs for new behaviour

---
+++ mapper/spec/integration/setup_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+RSpec.describe 'Mapper Setup' do
+  include_context 'container'
+
+  before do
+    configuration.mappers do
+      define(:users_mapper) do
+        attribute :id
+      end
+
+      define(:tags_mapper) do
+        attribute :user_id
+        attribute :label
+      end
+    end
+  end
+
+  it 'accessible through register_as with the same name as use in the define method' do
+    expected_result = [:users_mapper, :tags_mapper]
+    expect(configuration.mapper_classes.map(&:register_as)).to eq expected_result
+  end
+end
