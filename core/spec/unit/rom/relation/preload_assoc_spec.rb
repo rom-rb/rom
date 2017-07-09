c39d0d976aa2a4792c7cd80ca3e219d9474dbb7a
Piotr Solnica <piotr.solnica+oss@gmail.com>
2017-07-09 18:45:07 +0200

    Add explicit specs for Relation#preload_assoc

---
+++ core/spec/unit/rom/relation/preload_assoc_spec.rb
@@ -0,0 +1,25 @@
+require 'rom/relation'
+
+RSpec.describe ROM::Relation, '#preload_assoc' do
+  subject(:users) do
+    ROM::Relation.new([], name: ROM::Relation::Name[:users])
+  end
+
+  let(:tasks) do
+    ROM::Relation.new([], name: ROM::Relation::Name[:tasks])
+  end
+
+  let(:assoc) do
+    double(:assoc)
+  end
+
+  it 'is auto-curried' do
+    expect(users.preload_assoc(assoc)).to be_curried
+  end
+
+  it 'returns preloaded relation by association' do
+    expect(assoc).to receive(:preload).with(users, tasks).and_return([])
+
+    expect(users.preload_assoc(assoc, tasks)).to eql([])
+  end
+end
