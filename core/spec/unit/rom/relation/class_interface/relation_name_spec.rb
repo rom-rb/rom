2d24dcbf35e8d78ad7ab02d7e7096469c1811515
Piotr Solnica <piotr.solnica+oss@gmail.com>
2017-07-21 13:37:19 +0200

    [core] add specs for Relation.relation_name

---
+++ core/spec/unit/rom/relation/class_interface/relation_name_spec.rb
@@ -0,0 +1,27 @@
+require 'rom/relation'
+
+RSpec.describe ROM::Relation, '.relation_name' do
+  context 'when schema is defined' do
+    subject(:relation_class) do
+      Class.new(ROM::Relation) do
+        schema(:users) do
+          attribute :name, ROM::Types::String
+        end
+      end
+    end
+
+    it 'returns relation name configured by schema' do
+      expect(relation_class.relation_name).to eql(ROM::Relation::Name[:users])
+    end
+  end
+
+  context 'when schema is not defined' do
+    subject(:relation_class) do
+      Class.new(ROM::Relation)
+    end
+
+    it 'raises error' do
+      expect { relation_class.relation_name }.to raise_error(ROM::MissingSchemaError)
+    end
+  end
+end
