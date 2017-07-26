2e1ac9e369954d450f429c7618639d7f484bd091
Nikita Shilnikov <fg@flashgordon.ru>
2017-07-26 21:20:39 +0300

    Add YARD for Relation#changeset

--- changeset/lib/rom/plugins/relation/changeset.rb
+++ changeset/lib/rom/plugins/relation/changeset.rb
@@ -24,7 +24,51 @@ module ROM
 
           # Create a changeset for a relation
           #
-          # @return [Changeset]
+          # @overload changeset(type, data)
+          #   Create a changeset of one of predefined types
+          #
+          #   @example creating a record
+          #     users.
+          #       changeset(:create, name: 'Jane').
+          #       commit
+          #     # => #<ROM::Struct::User id=1 name="Jane">
+          #
+          #   @example updating a record
+          #     users.
+          #       by_pk(1).
+          #       changeset(:update, name: 'Jack').
+          #       commit
+          #     # => #<ROM::Struct::User id=1 name="Jane">
+          #
+          #   @example providing data as a separate step
+          #     changeset = users.changeset(:create)
+          #     jack = changeset.data(name: 'Jack').commit
+          #     jane = changeset.data(name: 'Jane').commit
+          #
+          #   @example using a command graph
+          #     users.
+          #       changeset(
+          #         :create,
+          #         name: "Jane",
+          #         posts: [{ title: "Something about aliens" }]
+          #       )
+          #
+          #   @param [Symbol] type The changeset type
+          #   @param [Hash] data
+          #   @return [Changeset]
+          #
+          # @overload changeset(changeset_class, data)
+          #   @example using a custom changeset class
+          #     class NewUser < ROM::Changeset::Create
+          #       map do |tuple|
+          #         { **tuple, name: tuple.values_at(:first_name, :last_name).join(' ') }
+          #       end
+          #     end
+          #
+          #     users.changeset(NewUser, first_name: 'John', last_name: 'Doe').commit
+          #
+          #   @param [Class] changeset_class A custom changeset class
+          #   @return [Changeset]
           #
           # @api public
           def changeset(type, data = EMPTY_HASH)
