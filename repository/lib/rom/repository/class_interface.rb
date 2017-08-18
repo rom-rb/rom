51342ca7d58ab10771443968fee6084281d30e94
GustavoCaso <gustavocaso@gmail.com>
2017-08-18 09:28:48 +0200

    Try a different approach for avoid Time Object been splatted

--- repository/lib/rom/repository/class_interface.rb
+++ repository/lib/rom/repository/class_interface.rb
@@ -113,13 +113,8 @@ module ROM
             if changeset.respond_to?(:commit)
               changeset.commit
             else
-              #   in case of view_args been coercible to an array we do not want to splat it
-              #   example: Date and Time objects
-              if view_args.respond_to?(:to_a)
-                root.command(type, **opts).public_send(view_name, view_args).call(*input)
-              else
-                root.command(type, **opts).public_send(view_name, *view_args).call(*input)
-              end
+              view_args = [view_args] unless view_args.respond_to?(:to_ary)
+              root.command(type, **opts).public_send(view_name, *view_args).call(*input)
             end
           end
         end
