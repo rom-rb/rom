47d084a52a1b5713c9d12c4d2ad81e5774f6efc7
Piotr Solnica <piotr.solnica+oss@gmail.com>
2017-11-13 11:00:49 +0100

    [core] fix ws in spec

--- core/spec/unit/rom/configurable_spec.rb
+++ core/spec/unit/rom/configurable_spec.rb
@@ -4,25 +4,25 @@ RSpec.describe ROM::Configurable do
   let (:klass) { Class.new { include ROM::Configurable } }
   let (:object) { klass.new }
   let (:config) { object.config }
-  
+
   it 'exposes the config property' do
     expect { config }.not_to raise_error
   end
-  
+
   it 'is configurable via block' do
     object.configure do |config|
       config.sql.infer_relations = false
     end
-    
+
     expect(config.sql.infer_relations).to be(false)
   end
-  
-  context ROM::Configurable::Config do  
+
+  context ROM::Configurable::Config do
     it 'can be traversed via dot syntax' do
       config.sql.infer_relations = false
       expect(config.sql.infer_relations).to be(false)
     end
-  
+
     it 'can be traversed via bracket syntax' do
       config[:sql].infer_relations = false
 
@@ -30,7 +30,7 @@ RSpec.describe ROM::Configurable do
       expect(config).to respond_to(:sql)
       expect(config.sql.infer_relations).to be(false)
     end
-  
+
     it 'freezes properly' do
       config.freeze
 
@@ -40,7 +40,7 @@ RSpec.describe ROM::Configurable do
     it 'handles unset keys when frozen' do
       config.sql.infer_relations = false
       config.freeze
-      
+
       expect(config.other).to be(nil)
       expect(config.key?(:other)).to be(false)
       expect(config.key?(:sql)).to be(true)
