f0a661c5ce339922c27e981d79773c7db9063a61
Piotr Solnica <piotr.solnica+oss@gmail.com>
2017-12-01 10:29:08 +0100

    [repository] cache root repository classes

---
+++ repository/spec/unit/repository/class_interface/root_builder_spec.rb
@@ -0,0 +1,17 @@
+require 'rom/repository'
+
+RSpec.describe ROM::Repository, '.[]' do
+  subject(:repo) do
+    Class.new(ROM::Repository)
+  end
+
+  let(:relation) { :users }
+
+  it 'creates a preconfigured ROM::Repository:Root class' do
+    expect(repo[relation].root).to be(relation)
+  end
+
+  it 'caches the class' do
+    expect(repo[relation]).to be(repo[relation])
+  end
+end
