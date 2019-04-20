1aa7de08a08471030cc23e2d32362504bc886251
Piotr Solnica <piotr.solnica@gmail.com>
2019-04-20 10:53:38 +0200

    Remove sql_overrides
    This was here temporarly while we worked on changes in multiple repos

--- changeset/spec/spec_helper.rb
+++ changeset/spec/spec_helper.rb
@@ -2,8 +2,6 @@ require 'pathname'
 
 SPEC_ROOT = Pathname(__FILE__).dirname
 
-require SPEC_ROOT.join('../../spec/sql_overrides')
-
 if RUBY_ENGINE == 'ruby' && ENV['COVERAGE'] == 'true'
   require 'yaml'
   rubies = YAML.load(File.read(SPEC_ROOT.join('../../.travis.yml')))['rvm']
