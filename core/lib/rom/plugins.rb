ca71777665eae9ba94cf6ce4fa6279d91d213c81
Nikita Shilnikov <fg@flashgordon.ru>
2019-06-15 10:14:32 +0300

    Make ROM::Plugins a container

--- core/lib/rom/plugins.rb
+++ core/lib/rom/plugins.rb
@@ -8,17 +8,12 @@ module ROM
   #
   # @api private
   module Plugins
-    @registry = ::Dry::Container.new
+    extend ::Dry::Container::Mixin
 
     class << self
       # @api private
       def register(entity_type, plugin_type: Plugin, adapter: true)
-        @registry.register(entity_type, plugin_type: plugin_type, adapter: adapter)
-      end
-
-      # @api private
-      def [](entity_type)
-        @registry[entity_type]
+        super(entity_type, plugin_type: plugin_type, adapter: adapter)
       end
     end
   end
