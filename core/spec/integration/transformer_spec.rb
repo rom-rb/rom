c5019960d5a2504786753d452e9a8b891abe67ae
Piotr Solnica <piotr.solnica@gmail.com>
2019-07-18 12:30:36 +0200

    [core] expand transformer spec to cover inheritance too

--- core/spec/integration/transformer_spec.rb
+++ core/spec/integration/transformer_spec.rb
@@ -2,11 +2,7 @@ require 'rom'
 require 'rom/transformer'
 
 RSpec.describe ROM::Transformer do
-  subject(:mapper) do
-    rom.mappers[:users][:default]
-  end
-
-  let(:relation) do
+  subject(:relation) do
     rom.relations[:users]
   end
 
@@ -14,11 +10,12 @@ RSpec.describe ROM::Transformer do
     ROM.container(:memory) do |config|
       config.relation(:users)
 
-      config.register_mapper(mapper_class)
+      config.register_mapper(default_mapper)
+      config.register_mapper(json_mapper)
     end
   end
 
-  let(:mapper_class) do
+  let(:default_mapper) do
     Class.new(ROM::Transformer) do
       relation :users, as: :default
 
@@ -28,9 +25,20 @@ RSpec.describe ROM::Transformer do
     end
   end
 
+  let(:json_mapper) do
+    Class.new(default_mapper) do
+      relation :users, as: :json
+
+      map do
+        deep_stringify_keys
+      end
+    end
+  end
+
   it 'works with rom container' do
     relation.insert(user_id: 1, name: 'Jane')
 
-    expect(relation.map_with(:default).to_a).to include(id: 1, name: 'Jane')
+    expect(relation.map_with(:default).to_a).to eql([id: 1, name: 'Jane'])
+    expect(relation.map_with(:json).to_a).to eql(['id' => 1, 'name' => 'Jane'])
   end
 end
