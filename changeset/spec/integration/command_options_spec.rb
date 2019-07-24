e55d10bc4ed954d537e9f7e16f05fb3ea8ec7a01
Piotr Solnica <piotr.solnica@gmail.com>
2019-07-24 11:57:22 +0200

    [changeset] add support for providing command plugin options

--- changeset/spec/integration/command_options_spec.rb
+++ changeset/spec/integration/command_options_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe 'changeset plugin' do
   let(:changeset_class) do
     Class.new(ROM::Changeset::Create) do
       command_options input: -> tuple { tuple.merge(updated_at: Time.now) }
+      command_plugins timestamps: { timestamps: [:created_at] }
     end
   end
 
@@ -17,6 +18,7 @@ RSpec.describe 'changeset plugin' do
   it 'extends the command with the provided command options' do
     book = changeset.commit
 
+    expect(book.created_at).to be_within(0.25).of(Time.now)
     expect(book.updated_at).to be_within(0.25).of(Time.now)
   end
 end
