d246cef54ddeb8576d4ad83bfbeb3e8263dc3423
Piotr Solnica <piotr.solnica@gmail.com>
2019-12-15 11:45:04 +0100

    Use codacy for coverage

--- repository/spec/spec_helper.rb
+++ repository/spec/spec_helper.rb
@@ -2,16 +2,6 @@ require 'pathname'
 
 SPEC_ROOT = Pathname(__FILE__).dirname
 
-if RUBY_ENGINE == 'ruby' && ENV['COVERAGE'] == 'true'
-  require 'yaml'
-  rubies = YAML.load(File.read(SPEC_ROOT.join('../../.travis.yml')))['rvm']
-  latest_mri = rubies.select { |v| v =~ /\A\d+\.\d+.\d+\z/ }.max
-
-  if RUBY_VERSION == latest_mri
-    require 'simplecov'
-  end
-end
-
 require 'rom-sql'
 require 'rom-repository'
 
