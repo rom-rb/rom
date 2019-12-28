6522c547b2cd0600e159b3fcf3d35221ad16fb00
Nikita Shilnikov <fg@flashgordon.ru>
2019-12-28 21:36:53 +0300

    Bump versions to 5.2.0

--- changeset/lib/rom/changeset/version.rb
+++ changeset/lib/rom/changeset/version.rb
@@ -2,6 +2,6 @@
 
 module ROM
   class Changeset
-    VERSION = '5.1.2'
+    VERSION = '5.2.0'
   end
 end
--- repository/lib/rom/repository/version.rb
+++ repository/lib/rom/repository/version.rb
@@ -2,6 +2,6 @@
 
 module ROM
   class Repository
-    VERSION = '5.1.2'
+    VERSION = '5.2.0'
   end
 end
