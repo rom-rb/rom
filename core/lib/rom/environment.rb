aadead27469b0983b4a37535a85a56bcb0a3426e
Nikita Shilnikov <fg@flashgordon.ru>
2025-01-02 10:35:32 +0100

    Remove version check

--- core/lib/rom/environment.rb
+++ core/lib/rom/environment.rb
@@ -49,11 +49,9 @@ module ROM
 
         if identifier.is_a?(Gateway)
           gateway = identifier
-        elsif RUBY_VERSION >= "3.0"
-          kwargs = args.last.is_a?(Hash) ? args.pop : {}
-          gateway = Gateway.setup(identifier, *args.flatten, **kwargs)
         else
-          gateway = Gateway.setup(identifier, *args.flatten)
+          kwargs = args.last.is_a?(::Hash) ? args.pop : {}
+          gateway = Gateway.setup(identifier, *args.flatten, **kwargs)
         end
 
         hash[:map][gateway] = name
