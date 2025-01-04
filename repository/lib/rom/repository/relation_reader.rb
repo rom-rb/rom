f426db6c6050053d60d0951e6be12ebda08a231f
Nikita Shilnikov <fg@flashgordon.ru>
2025-01-04 18:47:30 +0100

    Revert "Optimise the repository relations reader"
    This reverts commit a39ac5c7640ee5c5f27d70a3271c7d57ed3f58b3.

--- repository/lib/rom/repository/relation_reader.rb
+++ repository/lib/rom/repository/relation_reader.rb
@@ -3,58 +3,28 @@
 module ROM
   class Repository
     # @api private
-    class RelationReader < ::Module
-      extend ::Dry::Core::ClassAttributes
-
+    class RelationReader < Module
       # @api private
       attr_reader :klass
 
       # @api private
       attr_reader :relations
 
-      defines :relation_readers
-
-      defines :mutex
-      mutex(::Mutex.new)
-
-      defines :relation_cache
-      relation_cache(::Concurrent::Hash.new)
-
       module InstanceMethods
         # @api private
         def set_relation(name) # rubocop:disable Naming/AccessorMethodName
           container
             .relations[name]
-            .with(auto_struct: auto_struct, struct_namespace: struct_namespace)
+            .with(auto_struct: auto_struct)
+            .struct_namespace(struct_namespace)
         end
-
-        def relation_reader(name, relation_cache)
-          key = [name, auto_struct, struct_namespace]
-          relation_cache[key] ||= set_relation(name)
-        end
-      end
-
-      # @api private
-      def mutex
-        ::ROM::Repository::RelationReader.mutex
       end
 
       # @api private
       def initialize(klass, relations)
-        super()
-
+        @klass = klass
         @relations = relations
-        mutex.synchronize do
-          unless self.class.relation_readers
-            self.class.relation_readers(
-              build_relation_readers(
-                relations,
-                self.class.relation_cache
-              )
-            )
-          end
-        end
-        klass.include self.class.relation_readers
+        define_readers!
       end
 
       # @api private
@@ -66,12 +36,10 @@ module ROM
       private
 
       # @api private
-      def build_relation_readers(relations, relation_cache)
-        ::Module.new do
-          relations.each do |name|
-            define_method(name) do
-              relation_reader(name, relation_cache)
-            end
+      def define_readers!
+        relations.each do |name|
+          define_method(name) do
+            @relations[name] ||= set_relation(name)
           end
         end
       end
